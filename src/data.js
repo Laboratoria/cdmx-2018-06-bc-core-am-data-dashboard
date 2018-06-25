6da13a9d4b85c967b408f2af720285d71c1994e7
GenesisMauries <exodo_54@hotmail.com>
2018-06-25 17:40:33 -0500

    Obtencion de data y declaración de primeras funciones en objeto global

---
+++ src/data.js
@@ -0,0 +1,32 @@
+window.computeStudentsStats= () => {
+
+};
+
+ window.computeGenerationsStats = ()=> {
+
+ };
+
+ window.sortStudents = () => {
+
+ };
+ window.filterStudents = () => {
+
+};
+
+
+
+window.computeStudentsStats = () => {
+
+};
+
+window.computeGenerationsStats = () => {
+
+};
+
+window.sortStudents = () => {
+
+};
+
+window.filterStudents = () => {
+
+}; 
