8d26ed1ce0f736d2d04a930095307ff565a3431d
GenesisMauries <exodo_54@hotmail.com>
2018-06-25 18:10:26 -0500

    Seccion de sedes con sus respectivos inputs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 //Todo lo que se muestre en pantalla va en este archivo
 let primerCaja = document.querySelector("#inputs"); //Trayendo caja de inputs de HTML
 let botonEntrar = document.getElementById("entrar"); //id boton entrar
+const pullInformation = () =>{ //Funcion para jalar la data
+    const link= 'https://alppacat.github.io/data-dashboard/data/laboratoria.json'; //Variable para jalar la data
+    fetch(link).then (response => response.json())
+    .then(datos => {
+        console.log(datos);
+        
+    })
+}
+ pullInformation();
 
 botonEntrar.addEventListener("click", event =>{ //Evento del botón login
     let usuario = document.getElementById("usuario").value; //Valor de usuario
@@ -12,16 +21,4 @@ botonEntrar.addEventListener("click", event =>{ //Evento del botón login
 };
 })
 
-  const pullInformation = () =>{ //Funcion para jalar la data
-    const link= 'https://alppacat.github.io/data-dashboard/data/laboratoria.json'; //Variable para jalar la data
-    fetch(link).then (response => response.json())
-    .then(datos => {
-        console.log(datos);
-        
-    })
-}
- pullInformation();
-
-
-
- 
+  
