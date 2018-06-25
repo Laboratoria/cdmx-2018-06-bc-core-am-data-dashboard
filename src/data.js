fe0956398dabb5d2c34eff2878626c64f43ff7f4
Judith <baruki110214@gmail.com>
2018-06-25 00:28:30 -0500

    posible soluciòn no corre

--- src/data.js
+++ src/data.js
@@ -1,7 +1,56 @@
 
 debugger;
+var requestURL = "https:/github.com/JudithCortes/cdmx-2018-06-bc-core-am-data-dashboard/blob/master/data/laboratoria.json";
+var request= new XMLHttpRequest();
+request.open("GET",requestURL);
+request.responseType="json"
+request.send();
+request.onload= function (){
+  var sedes=request.response;
+  populateHeader(sedes);
+  showSedes(sedes);
+}
+function populateHeader(jsonObj) {
+  var myH1 = document.createElement('h1');
+  myH1.textContent = jsonObj['generacion'];
+  header.appendChild(myH1);
 
-$(documento).ready(function(){
+  var myPara = document.createElement('p');
+  myPara.textContent = 'Generacion: ' + jsonObj['generacion'] + ' // Formed: ' + jsonObj['formed'];
+  header.appendChild(myPara);
+}
+function showSedes(jsonObj) {
+  var Sede = jsonObj['generacion'];
+
+  for (var i = 0; i < sede.length; i++) {
+    var myArticle = document.createElement('article');
+    var myH2 = document.createElement('h2');
+    var myPara1 = document.createElement('p');
+    var myPara2 = document.createElement('p');
+    var myPara3 = document.createElement('p');
+    var myList = document.createElement('ul');
+
+    myH2.textContent = sede[i].name;
+    myPara1.textContent = 'generacion: ' + sede[i].generacion;
+
+    var sedeGene = sedes[i].powers;
+    for (var j = 0; j < sedeGene.length; j++) {
+      var listItem = document.createElement('li');
+      listItem.textContent = sedeGene[j];
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
+
+/*$(documento).ready(function(){
 $.getJSON("imprime", function(response){
   console.log(JSON.stringfy(response));
 });
