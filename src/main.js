0a3098f7643779ceb57ccbb179110a979294eac5
soberanesmajo <soberanesmajo@gmail.com>
2018-06-27 09:34:04 -0500

    Se modifica el color de la interfaz. Logo de Laboratoria añadido.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
 
-const gettingData= ()=> {
-    let url = "https://raw.githubusercontent.com/DianaCarrillo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
-
-    fetch(url).then(data => data.json())
-    .then(data => {
-        console.log(data);
-    });
-}
-
-gettingData();
 
 
 let sede =document.getElementById("ciudad-generacion");
