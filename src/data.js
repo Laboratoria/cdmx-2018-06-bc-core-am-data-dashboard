644560a946f59b4b98855b4186c745b748a10883
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-06-28 17:24:47 -0500

    cambios en main js, boton html e ingreso funcionando

--- src/data.js
+++ src/data.js
@@ -1,22 +1,17 @@
 
-const gettingData= ()=> {
-    let url = "https://raw.githubusercontent.com/DianaCarrillo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
+// const gettingData= ()=> {
+//     let url = "https://api.myjson.com/bins/fjbkm"
+//     let laboratoria = fetch(url);
 
-    fetch(url).then(data => data.json())
-    .then(data => {
-        console.log(data);
-    });
-}
-
-gettingData();
+//     then(data => data.json())
+//     .then(data => {
+//         console.log(data);
+//     });
+// }
+// gettingData();
 
 
 window.computeStudentsStats = () => {
-
-    let student = [ 
-        {
-            name: 
-        }
 
 };
 
