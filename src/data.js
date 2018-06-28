413b66f897fcc12d40c98b97685683600a0dcb42
emeraldng <ifglitterthen@gmail.com>
2018-06-28 00:40:29 -0500

    data.js

---
+++ src/data.js
@@ -0,0 +1,19 @@
+window.computeStudentsStats = {
+    (laboratoria, api) => {
+
+    };
+    //aquí va el código
+};
+
+window.computeGenerationsStats = {
+    //aquí va el código
+};
+
+window.sortStudents = {
+
+};
+
+window.filterStudents = {
+    
+};
+
---
+++ src/main.js
@@ -0,0 +1,3 @@
+let api = "https://api.myjson.com/bins/1efx86"
+const button = document.getElementById('jsonBtn');
+button.addEventListener('click', cargarJSON);
