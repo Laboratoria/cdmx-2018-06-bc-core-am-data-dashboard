71ba5eb79797274026979de2543de40e6fadf37c
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-06-29 09:55:06 -0500

    fetch

--- src/data.js
+++ src/data.js
@@ -1,9 +1,25 @@
 /*Código que tenga que ver con  funciones que  obtienen y manipulan los datos*/
-const url = `https://raw.githubusercontent.com/brenduchiz/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`;
-fetch(url)
+window.datadashboard = {
+
+computeStudentsStats: (laboratoria) => {
+fetch(`https://raw.githubusercontent.com/brenduchiz/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`)
 .then(respuesta => respuesta.json())
-.then (respuesta => {
+.then ( data=> {
+
+tabla(data)
+})
 
 
-console.log(respuesta)
- })
+function tabla(data) {
+
+let estudiante = data.lima.generacion.cuarta.estudiantes;
+for (var i = 0; i < estudiante.length; i++) {
+let nombre_tercera=  estudiante[i].nombre;
+console.log(nombre_tercera);
+laboratoria.innerHTML = (nombre_tercera);
+}
+
+
+}
+}
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 /*Código que tenga que ver con mostrar los datos en la pantalla*/
+let studentsimp=document.getElementById("pintar_estudiantes");
+let tercer=document.getElementById("lima_tercera");
+
+tercer.addEventListener("click",evento=>{window.datadashboard.computeStudentsStats(studentsimp)});
