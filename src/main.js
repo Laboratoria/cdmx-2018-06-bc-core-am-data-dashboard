edcf3d7ff4e6792ce571ef6b589ba96849e4ab4e
VaniushaR <vaniusha.ra@gmail.com>
2018-06-29 11:51:52 -0500

    Json en consola

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
-// Llamar a la base de datos en objeto json
+/* Llamar a la base de datos en objeto json
 let datos = "https://raw.githubusercontent.com/VaniushaR/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
 // Declarar variables globales
 let sede = document.getElementById("sede");
 let generacion = document.getElementById("generación");
-let nombre = document.getElementById("nombre");
-let btn = document.getElementById("btn");
+let nombre = document.getElementById("nombre"); */
+
+
 
 
 //Llamar al DOM con click
-btn.addEventListener("click", function (){
-    clicking();
-})
+
 
 
 
