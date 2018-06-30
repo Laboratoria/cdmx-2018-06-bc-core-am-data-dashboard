78239eb552b373796ec6a0124f8010f5d81553e8
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-06-30 16:21:48 -0500

    version finalizada de login

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-let sede =document.getElementsByClassName("col-12");
+let sede =document.getElementById("buttons");
 let ingresar =document.getElementById("ingresar");
 let inputName = document.getElementById("input-name");
 let password = document.getElementById("input-password");
@@ -9,9 +9,9 @@ sede.style.display="none";
 
 ingresar.addEventListener("click", obtenerDatos = ()=> { 
     let usuario1 = inputName.value;
-     console.log(usuario1);
+    //  console.log(usuario1);
     let password1 = password.value;
-     console.log(password1);
+    //  console.log(password1);
         if (usuario1==="lucile" && password1==="123"){
         sede.style.display="block";
         form.style.display="none";
@@ -20,5 +20,23 @@ ingresar.addEventListener("click", obtenerDatos = ()=> {
         alert("Usuario no identificado");
     }
 });
+
+
+const json = '../data/laboratoria.json';// sabemos que tiene que haber un fetch para "jalar" la info. de la carpeta laboratoria.json, pero como está en un servidor necesitamos cargarlo. y cerrarlo después
+
+const getData = () => {// cuando jale la información, quiero que la convierta en json con el método .json()
+    fetch(json).then (response => response.json())// haciendo fetch de json y aplicando las promesas .then para que realicen una acción (función)
+    .then((res)=>{
+        const generations = computeGenerationStats(res);
+        const users= computeStudentsStats(res);
+        drawCampus(generations);
+    })
+};
+getData();
+    
+
+const drawCampus = (data) => {
+    const sedes = Object.keys(data)
+}
 
 
