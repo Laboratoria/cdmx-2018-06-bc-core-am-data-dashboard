0e6e831027b7e64e871acf48e51278072a0f225b
Mónica Pavía <monicelica_pavi@hotmail.com>
2018-06-30 00:35:24 -0500

    Se modifico el main.js para poder pintar sedes y se logro pintar generaciones de Lima, tambien entrar al objeto de alumnas

--- src/main.js
+++ src/main.js
@@ -3,11 +3,40 @@
 let url = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
 //crear let para poder pintar datos
 let showSedes= document.querySelector("#generaciones");
+let showAlumnas= document.querySelector("#alumnas");
 // a través del boton iniciar funcion
+
+//SEDES
+sedesAll.addEventListener("click", function(){
+  generacion(); 
+})
+//GENERACIONES
 sed.addEventListener("click", function(){
   sede(); 
 })
 
+//ALUMNAS
+
+alumnasA.addEventListener("click", function(){
+  alumnas(); 
+})
+
+
+let generacion = function(){
+  fetch(url).then(function(datos){
+  return datos.json();
+  }).then(function(data){
+  console.log(data);
+  let generaciones= data;
+  console.log(Object.keys(generaciones));
+  let generacionesLima = Object.keys(generaciones);
+  console.log(generacionesLima[0]);
+  for(let i = 0; i<generacionesLima.length; i++) {
+        showSedes.innerHTML+= "<div>" + generacionesLima[i] + "</div>";
+    }
+
+  })
+}
 
 
 let sede = function(){
@@ -27,6 +56,22 @@ let sede = function(){
 }
 
 
+
+let alumnas = function(){
+  fetch(url).then(function(datos){
+  return datos.json();
+  }).then(function(data){
+  console.log(data.lima);
+  let sedeLimaGeneracion= data.lima.generacion.cuarta.estudiantes;
+  console.log(Object.keys(sedeLimaGeneracion));
+  let generacionesLimaAlumanas = Object.keys(sedeLimaGeneracion);
+  console.log(generacionesLimaAlumanas[0]);
+  for(let i = 0; i<generacionesLimaAlumanas.length; i++) {
+        showAlumnas.innerHTML+= "<div>" + generacionesLimaAlumanas[i] + "</div>";
+    }
+
+  })
+}
 
 
 
