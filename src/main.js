375ddc390da07c6f0e91dc51873e6eb285875ef5
Mónica Pavía <monicelica_pavi@hotmail.com>
2018-06-30 01:16:36 -0500

    Se trabajo con los datos de alumnas,logrando pintar nombre y correo

--- src/main.js
+++ src/main.js
@@ -2,17 +2,20 @@
 //url para obtener el json
 let url = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
 //crear let para poder pintar datos
+let showGeneraciones= document.querySelector("#sedes");
 let showSedes= document.querySelector("#generaciones");
 let showAlumnas= document.querySelector("#alumnas");
+let nombre= document.querySelector("#nombre");
+let correo = document.querySelector("#correo");
 // a través del boton iniciar funcion
 
 //SEDES
 sedesAll.addEventListener("click", function(){
-  generacion(); 
+  sede(); 
 })
 //GENERACIONES
 sed.addEventListener("click", function(){
-  sede(); 
+  generacion(); 
 })
 
 //ALUMNAS
@@ -22,34 +25,34 @@ alumnasA.addEventListener("click", function(){
 })
 
 
-let generacion = function(){
+let sede = function(){
   fetch(url).then(function(datos){
   return datos.json();
   }).then(function(data){
   console.log(data);
-  let generaciones= data;
-  console.log(Object.keys(generaciones));
-  let generacionesLima = Object.keys(generaciones);
-  console.log(generacionesLima[0]);
-  for(let i = 0; i<generacionesLima.length; i++) {
-        showSedes.innerHTML+= "<div>" + generacionesLima[i] + "</div>";
+  let sedes= data;
+  console.log(Object.keys(sedes));
+  let allSedes = Object.keys(sedes);
+  console.log(allSedes[0]);
+  for(let i = 0; i<allSedes.length; i++) {
+        showSedes.innerHTML+= "<div>" + allSedes[i] + "</div>";
     }
 
   })
 }
 
 
-let sede = function(){
+let generacion = function(){
   fetch(url).then(function(datos){
   return datos.json();
   }).then(function(data){
   console.log(data.lima);
   let sedeLima= data.lima.generacion;
   console.log(Object.keys(sedeLima));
-  let generacionesLima = Object.keys(sedeLima);
-  console.log(generacionesLima[0]);
-  for(let i = 0; i<generacionesLima.length; i++) {
-        showSedes.innerHTML+= "<div>" + generacionesLima[i] + "</div>";
+  let sedesLima = Object.keys(sedeLima);
+  console.log(sedesLima[0]);
+  for(let i = 0; i<sedesLima.length; i++) {
+        showSedes.innerHTML+= "<div>" + sedesLima[i] + "</div>";
     }
 
   })
@@ -61,14 +64,29 @@ let alumnas = function(){
   fetch(url).then(function(datos){
   return datos.json();
   }).then(function(data){
-  console.log(data.lima);
-  let sedeLimaGeneracion= data.lima.generacion.cuarta.estudiantes;
-  console.log(Object.keys(sedeLimaGeneracion));
+  //OBTENER NOMBRE
+  console.log(data.lima.generacion.cuarta.estudiantes[0].nombre);
+  let nombreLima = data.lima.generacion.cuarta.estudiantes[0].nombre;
+  nombre.innerHTML = nombreLima;
+  /*console.log(data.lima.generacion.cuarta.estudiantes.nombre);
+  let nombreLima = data.lima.generacion.cuarta.estudiantes.nombre;
+  console.log(Object.keys(nombreLima));
+  let allnombres = Object.keys(nombreLima);
+  console.log(allnombres[0]);
+  for(let i = 0; i<allnombres.length; i++) {
+    nombre.innerHTML+= "<div>" + allnombres[i] + "</div>";
+  }*/
+  //OBTENER CORREO
+  console.log(data.lima.generacion.cuarta.estudiantes[0].correo);
+   let correoLima = data.lima.generacion.cuarta.estudiantes[0].correo;
+    correo.innerHTML = correoLima;
+  
+  /*console.log(Object.keys(sedeLimaGeneracion));
   let generacionesLimaAlumanas = Object.keys(sedeLimaGeneracion);
   console.log(generacionesLimaAlumanas[0]);
   for(let i = 0; i<generacionesLimaAlumanas.length; i++) {
         showAlumnas.innerHTML+= "<div>" + generacionesLimaAlumanas[i] + "</div>";
-    }
+    }*/
 
   })
 }
