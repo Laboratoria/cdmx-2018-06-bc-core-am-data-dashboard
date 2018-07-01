37e91cdd8e79bf58ae0063792b24ac502bcbefbb
Adriana Mora <AMHymyilee@users.noreply.github.com>
2018-07-01 14:51:50 -0500

    añadiendo readme y js login

--- src/main.js
+++ src/main.js
@@ -1,49 +1,91 @@
-
 //url para obtener el json
-let apiUrl = "https://raw.githubusercontent.com/monicelica/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+let url = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+//crear let para poder pintar datos
+let showGeneraciones = document.querySelector("#sedes");
+let showSedes = document.querySelector("#generaciones");
+let showAlumnas = document.querySelector("#alumnas");
+let nombre = document.querySelector("#nombre");
+let correo = document.querySelector("#correo");
 // a través del boton iniciar funcion
-sed.addEventListener("click", function(){
-  sede(); 
+
+//SEDES
+sedesAll.addEventListener("click", function () {
+  sede();
+})
+//GENERACIONES
+sed.addEventListener("click", function () {
+  generacion();
+})
+
+//ALUMNAS
+
+alumnasA.addEventListener("click", function () {
+  alumnas();
 })
 
 
+let sede = function () {
+  fetch(url).then(function (datos) {
+    return datos.json();
+  }).then(function (data) {
+    console.log(data);
+    let sedes = data;
+    console.log(Object.keys(sedes));
+    let allSedes = Object.keys(sedes);
+    console.log(allSedes[0]);
+    for (let i = 0; i < allSedes.length; i++) {
+      showSedes.innerHTML += "<div>" + allSedes[i] + "</div>";
+    }
 
-let sede = function(){
-	fetch(apiUrl).then(function(datos){
-	return datos.json();
-	}).then(function(data){
-	console.log(data);
-	})
+  })
+}
+
+
+let generacion = function () {
+  fetch(url).then(function (datos) {
+    return datos.json();
+  }).then(function (data) {
+    console.log(data.lima);
+    let sedeLima = data.lima.generacion;
+    console.log(Object.keys(sedeLima));
+    let sedesLima = Object.keys(sedeLima);
+    console.log(sedesLima[0]);
+    for (let i = 0; i < sedesLima.length; i++) {
+      showSedes.innerHTML += "<div>" + sedesLima[i] + "</div>";
+    }
+
+  })
 }
 
 
 
+let alumnas = function () {
+  fetch(url).then(function (datos) {
+    return datos.json();
+  }).then(function (data) {
+    //OBTENER NOMBRE
+    console.log(data.lima.generacion.cuarta.estudiantes[0].nombre);
+    let nombreLima = data.lima.generacion.cuarta.estudiantes[0].nombre;
+    nombre.innerHTML = nombreLima;
+    /*console.log(data.lima.generacion.cuarta.estudiantes.nombre);
+    let nombreLima = data.lima.generacion.cuarta.estudiantes.nombre;
+    console.log(Object.keys(nombreLima));
+    let allnombres = Object.keys(nombreLima);
+    console.log(allnombres[0]);
+    for(let i = 0; i<allnombres.length; i++) {
+      nombre.innerHTML+= "<div>" + allnombres[i] + "</div>";
+    }*/
+    //OBTENER CORREO
+    console.log(data.lima.generacion.cuarta.estudiantes[0].correo);
+    let correoLima = data.lima.generacion.cuarta.estudiantes[0].correo;
+    correo.innerHTML = correoLima;
 
-
-
-/*let clicking = function(){
-  fetch(api).then(function(datos){
-  return datos.json();
-  }).then(function(data){
-  console.log(data.results[0].name.first);
-    let nombre = data.results[0].name.first; 
-    let apellido = data.results[0].name.last;
-    let fullnameAPI = nombre + ' ' + apellido;
-    
-    fullname.innerHTML = fullnameAPI;
-    
-    
-    let emailAPI = data.results[0].email;
-    email.innerHTML = emailAPI;
-    
-    let usernameAPI = data.results[0].login.username;
-    username.innerHTML = usernameAPI;
-    
-    let cityAPI = data.results[0].location.city;
-    city.innerHTML = cityAPI;
-    
-    let avatarAPI = data.results[0].picture.large;
-    avatar.setAttribute("src", avatarAPI);
+    /*console.log(Object.keys(sedeLimaGeneracion));
+    let generacionesLimaAlumanas = Object.keys(sedeLimaGeneracion);
+    console.log(generacionesLimaAlumanas[0]);
+    for(let i = 0; i<generacionesLimaAlumanas.length; i++) {
+          showAlumnas.innerHTML+= "<div>" + generacionesLimaAlumanas[i] + "</div>";
+      }*/
 
   })
-} */    
+}
