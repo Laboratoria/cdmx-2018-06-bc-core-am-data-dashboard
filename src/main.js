a8eda441f5fcf085b09a2de9e5dce86ba6873e64
VaniushaR <vaniusha.ra@gmail.com>
2018-07-02 09:19:39 -0500

    Avances de objeto .window y esqueleto de funciones

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
-/* Llamar a la base de datos en objeto json
-let datos = "https://raw.githubusercontent.com/VaniushaR/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
-// Declarar variables globales
-let sede = document.getElementById("sede");
-let generacion = document.getElementById("generación");
-let nombre = document.getElementById("nombre"); */
+//Login provisional
+const usuaria = "Lucile";
+const password = "Devendra";
+//se declaran las variables que contendrán la entrada de los inputs
+let inputUser = document.getElementById("user-name");
+let passwordUser = document.getElementById("password");
 
-
-
-
-//Llamar al DOM con click
-
-
-
-
-//Llamar a los arreglos generaciones depende la sede elegida anteriormente
+function login (usuaria, pasword) {
+    if (inputUser == usuaria && pasword == passwordUser) {
+        return document.getElementById("welcome").write.usuaria
+    }
+    
+}
+//Asignar variable a laboratoria.json desde el localhost
+const laboratoria = "../data/laboratoria.json";
