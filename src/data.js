65cc08fb7d1298ef76e593cd189a3f3fd566c195
MiriamGaGu <mgg_pjl@yahoo.com>
2018-07-02 15:58:21 -0500

    Last changes

--- src/data.js
+++ src/data.js
@@ -1,41 +1,95 @@
-//Aqui van las funciones globles.
-window.computeStudentsStats = (laboratoria) => {
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
 
 }
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+let api = "https://api.myjson.com/bins/vv6gi";
+
+function tableData (data) {
+    
+    //Creates the header using keys from JSON
+
+    let objectKeys = Object.keys(data[0]);
+    let table = document.querySelector('table');
+    let headerRow = document.createElement ('tr');
+    
+    for (i=0; i < objectKeys.length; i++) {
+        let headerCell = document.createElement('th');
+        if (objectKeys[i].startsWith("first")) {
+            headerCell.append(document.createTextNode("Not First Name"));
+        } else {
+            headerCell.append(document.createTextNode(objectKeys[i]));
+        }
+        
+        headerRow.append(headerCell);
+    };
+    
+    table.append(headerRow);
+    
+};
 
 
-window.computeGenerationsStats = (laboratoria) => {
-
-}
-
-window.sortStudents = (students, orderBy, orderDirection) => {
-
-}
 
 
-window.filterStudents = (students, search) => {
+//Get JSON, run function
 
-}
+fetch(api)
+        .then(function (response) {
+        return response.json();
+    }).then(function (json) {
+       tableData(json);
+});
 
+// Function
 
-
-const Alumnas = document.getElementById('jsonBtn');
-Alumnas.addEventListener('click', cargarJSON);
-
-//console.log(Alumnas);
-let api = "https://api.myjson.com/bins/1efx86"
-
-function cargarJSON() {
-   fetch(api)
-   .then(function(res) {
-       return res.json();
-       console.log(res);
-
-   })
-   .then(function(data) {
-       let nombre1 = (data.lima.generacion.cuarta.estudiantes[0].nombre);
-       //console.log("nombre1");
-       document.getElementById('resultado').innerHTML = nombre1;
-   })
-
-}
+    function tableData (data) {
+        
+        // Create Headings
+        
+        let objectKeys = Object.keys(data[0]);
+        let table = document.querySelector('table');
+        let headerRow = document.createElement ('tr');
+        
+        for (let keyName of objectKeys) {
+               let headerCell = document.createElement('th');
+               headerCell.append(document.createTextNode(keyName));
+               headerRow.append(headerCell);
+           };
+        
+        table.append(headerRow);
+        
+        // Table Data
+        
+        for (let i = 0; i < data.length; i++) {
+               let dataRow = document.createElement('tr');
+               
+               for (let keyName of objectKeys) {
+                   let dataCell = document.createElement('td');
+                   if (data[i][keyName] === null) {
+                       dataCell.append(document.createTextNode("Not Known"));
+                   } else if (data[i][keyName].startsWith("data:image")) {
+                       let img = document.createElement('img');
+                       img.src = data[i][keyName];
+                       dataCell.append(img);
+                   } else {
+                       dataCell.append(document.createTextNode(data[i][keyName]));
+                   };
+                   
+                   dataRow.append(dataCell);
+               };
+               
+               table.append(dataRow);
+           }
+       };
