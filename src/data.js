b7664f87c9b9170bcaa143fbc6a7c8f1f1f7702f
unknown <nan.si_1305@hotmail.com>
2018-07-02 12:47:11 -0500

    muestra sedes y generaciones, per aun nos falta mostrar a las estudiantes

--- src/data.js
+++ src/data.js
@@ -1,9 +1,35 @@
-let keys = Object.keys(data); //asigno el objeto data en la variable keys, con objet.keys obtengo todo el data
-let select = document.getElementById('selectSedes'); //selecSedes es el seleccionador de sedes y desde ahi parto a hacer el rastreo de la info de la data
+let laboratoria = fetch("https://raw.githubusercontent.com/NancyyM/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json")
+  .then((response) => response.json())
+  .then((dataJSON) => {
+    return dataJSON;
+  });
 
-for (let i = 0; i < keys.length; i++) { //recorre todo el objeto y me permite obtener la informacion
-  	let opt = document.createElement('option'); //se guarda en opt
-    opt.value = keys[i]; //es la informacion en determinada posicion y se asigna a opt
-    opt.innerHTML = keys[i]; 
-    select.appendChild(opt); //El método appendChild() inserta un nuevo nodo dentro de la estructura DOM de un documento, y es la segunda parte del proceso central uno-dos, crear-y-añadir para construir páginas web a base de programación
-}; 
+const loadsedes = () => {
+  laboratoria.then(function(dataJSON) {
+    //console.log(dataJSON);
+    let keys = Object.keys(dataJSON); //asigno el objeto data en la variable keys, con objet.keys obtengo todo el data
+    console.log(keys);
+    let select = document.getElementById('selectSedes'); //selecSedes es el seleccionador de sedes y desde ahi parto a hacer el rastreo de la info de la data
+    for (let i = 0; i < keys.length; i++) { //recorre todo el objeto y me permite obtener la informacion
+        let opt = document.createElement('option'); //se guarda en opt
+        opt.value = keys[i]; //es la informacion en determinada posicion y se asigna a opt
+        opt.innerHTML = keys[i];
+        select.appendChild(opt); //El método appendChild() inserta un nuevo nodo dentro de la estructura DOM de un documento, y es la segunda parte del proceso central uno-dos, crear-y-añadir para construir páginas web a base de programación
+    };
+  });
+}
+const loadgeneraciones = (sede,select) => {
+  laboratoria.then(function(dataJSON) {
+    let keys = Object.keys(dataJSON[sede]['generacion']);//obtenemos los keys del objeto data[valor]['generacion'] los cuales son keys = [cuarta,quinta,tercera]
+    var opt = document.createElement('option');//aqui creamos un elemento option que es la para el select(lista) de generaciones
+    opt.value = 0;//asignamos el valor de 0 al primer valor que tendra el select(lista) de generaciones
+    opt.innerHTML = "";//EL texto que mostrara el select(lista) de generaciones
+    select.appendChild(opt);//creamos el valor y el texto al final del elemento ejemplo <option value="0"></option>
+    for (let i = 0; i < keys.length; i++) {//recorremos los keys del objeto data[valor]['generacion']
+        opt = document.createElement('option');//aqui creamos un elemento option que es la para el select(lista) de generaciones
+        opt.value = keys[i];//le asignamos el valor con el incide del elemento que le corresponda ejemplo keys[0]= tercera, esto se hace para poder tomar el valor despues y saber que generacion estan seleccionando
+        opt.innerHTML = keys[i];//le asignamos el mismo valor en el texto keys[0]= tercera
+        select.appendChild(opt);//creamos el valor y el texto al final del elemento ejemplo <option value="cuarta">cuarta</option>
+    };
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,33 +1,24 @@
-const generation = (e) => { //FUNCION GENERACION DE ACUERDO A LA SEDE SELECCIONADA
+//loadsedes();
+const generation = () => { //FUNCION GENERACION DE ACUERDO A LA SEDE SELECCIONADA
 	document.getElementById('generation').innerHTML = ""; //Limiamps el select de generaciones cada vez que se seleccione una nueva sede
 	document.getElementById('tabla').innerHTML = ""; //Limpiamos la tabla cada vez que se cambie la sede
 	let valor = document.getElementById('selectSedes').value; //obtenemos el valor de la lista de sedes seleccionada, con el evento onchange detectamos cada vez que cambie la lista de sedes
 	let select = document.getElementById('generation'); //guardamos el elemento select de generatoion en la variable select para poder hacer el contenido y llenarlo con la lista de generaciones
-	let keys = Object.keys(data[valor]['generacion']);//obtenemos los keys del objeto data[valor]['generacion'] los cuales son keys = [cuarta,quinta,tercera]
-	var opt = document.createElement('option');//aqui creamos un elemento option que es la para el select(lista) de generaciones
-	opt.value = 0;//asignamos el valor de 0 al primer valor que tendra el select(lista) de generaciones
-	opt.innerHTML = "";//EL texto que mostrara el select(lista) de generaciones
-	select.appendChild(opt);//creamos el valor y el texto al final del elemento ejemplo <option value="0"></option> 
-	for (let i = 0; i < keys.length; i++) {//recorremos los keys del objeto data[valor]['generacion']
-	  	opt = document.createElement('option');//aqui creamos un elemento option que es la para el select(lista) de generaciones
-	    opt.value = keys[i];//le asignamos el valor con el incide del elemento que le corresponda ejemplo keys[0]= tercera, esto se hace para poder tomar el valor despues y saber que generacion estan seleccionando
-	    opt.innerHTML = keys[i];//le asignamos el mismo valor en el texto keys[0]= tercera
-	    select.appendChild(opt);//creamos el valor y el texto al final del elemento ejemplo <option value="cuarta">cuarta</option>
-	};
+	loadgeneraciones(valor,select);
 
 }
 
-const students = (e) => {
+const students = () => {
 	let valor = document.getElementById('selectSedes').value;//obtenemos el valor de las sedes, la sede que se selecciono
 	let generation = document.getElementById('generation').value;//obtenemos el valor de las generaciones, la que se ha seleccionado
-	let estudiantes = data[valor]['generacion'][generation]['estudiantes'];//obtenemos el objeto basandonos en la informacion obtenida y accediendo a los keys ejmplo data[mexico]['generacion'][cuarta]['estudiantes']
+	let estudiantes = dataJSON[valor]['generacion'][generation]['estudiantes'];//obtenemos el objeto basandonos en la informacion obtenida y accediendo a los keys ejmplo data[mexico]['generacion'][cuarta]['estudiantes']
 
 	let keys = Object.keys(estudiantes);//obtenemos los keys del objeto antes guardado en estudiantes ejemplo [0,1,2,3,......]
 	let keysprogreso = '';//cramos variables para usarlas despues seran los keys de los progresos, ya que este es un arreglo
 	let duracionPrograma = '';//cramos variables para usarlas despues para guardar el valor de duracionprograma
 	let porcentajeCompletado = '';//cramos variables para usarlas despues para guardar porcentajecompletado
 	document.getElementById('tabla').innerHTML = "";//limpiamos la tabla cada vez que cambien el valor del select(lista) de generaciones y asi poder mostrar la nueva informacion de la generaciones seleccionada
-	
+
 
 	//Se crea tabla
 	var table = document.getElementById('tabla');//obtenemos el elemento tabla del html, para poder crear su contenido despues.
@@ -35,7 +26,7 @@ const students = (e) => {
 	let titlenombre = document.createTextNode('Nombre');//creamos un nuevo nodo de texto que se convertira en el titulo de nombre
 	let titleprograma = document.createTextNode('Duración del programa');//creamos un nuevo nodo de texto que se convertira en el titulo de Duracion del programa
 	let titlecompletado = document.createTextNode('Porcentaje Completado');//creamos un nuevo nodo de texto que se convertira en el titulo de Porcentaje COmpletado
-	let tr1 = document.createElement('tr');//creamos el nuevo elemento tr perteneciente de la tabla y lo guardamos en la variable tr  
+	let tr1 = document.createElement('tr');//creamos el nuevo elemento tr perteneciente de la tabla y lo guardamos en la variable tr
 	let td10 = document.createElement('td');//creamos el td para poder guardar la informacion del texto de los titulos,
 	let td11 = document.createElement('td');//creamos el td para poder guardar la informacion del texto de los titulos,
 	let td12 = document.createElement('td');//creamos el td para poder guardar la informacion del texto de los titulos,
@@ -51,7 +42,7 @@ const students = (e) => {
 	table.appendChild(tr1);//asignamos el valor de tr1 en el ultimo elemento de la tabla para que se pueda visualizar, ejemplo de lo que crea <tr><td>Correo</td><td>Nombre</td><td>Duración del programa</td><td>Porcentaje Completado</td></tr>
 	for (let i = 0; i < keys.length; i++) {
 		/*console.log(estudiantes.indexOf(i));*/
-	  	let tr = document.createElement('tr');//creamos el nuevo elemento tr perteneciente de la tabla y lo guardamos en la variable tr     
+	  	let tr = document.createElement('tr');//creamos el nuevo elemento tr perteneciente de la tabla y lo guardamos en la variable tr
 	    let td1 = document.createElement('td');//creamos el td para poder guardar la informacion del texto de los estudiantes
 	    let td2 = document.createElement('td');//creamos el td para poder guardar la informacion del texto de los estudiantes
 	    let td3 = document.createElement('td');//creamos el td para poder guardar la informacion del texto de los estudiantes
