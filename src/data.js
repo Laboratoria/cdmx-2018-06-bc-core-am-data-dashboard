8ebdd17f2dd6f72c1940b99dbd2c4a88ea486864
LAPTOP-8T72OT84\Lucia <rocioa.matias@gmail.com>
2018-07-02 12:48:17 -0500

    Mostrando alumnas

--- src/data.js
+++ src/data.js
@@ -1,12 +1,45 @@
 window.global = {
-  computeStudentsStats: () => {
+  computeStudentsStats: (laboratoria) => {
     const url = `https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`;
     fetch(url)
-      .then(res => res.json())
+      .then(respuesta => respuesta.json())
       .then(data => {
-        console.log(data);
+        const generaciones = data.lima.generacion;
+        estudiante(data)
       })
-  }
+    }
+  };
 
-};
-//
+     estudiante = (data) => {
+      let estudiante = data.lima.generacion.cuarta.estudiantes;
+      let result = "";
+      for (let i = 0; i <estudiante.length; i++) {
+        let nombres = estudiante[i];
+        //console.log(nombres, pintar);
+        result = result + `
+          <tr>
+            <th scope="row">1</th>
+            <td>${nombres.correo}</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>${estudiante.nombre}</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>${nombres.turno}</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+
+`
+
+
+      }
+      pintar.innerHTML = result;
+    }
