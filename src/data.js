eda6b3c2b9570c3b326aaa950133808708703cbb
MirEnovi <garciaivonne11@gmail.com>
2018-07-03 07:07:21 -0500

    Se modifica funciones que pintan estudiantes y en consola se muestra información de generaciones

--- src/data.js
+++ src/data.js
@@ -1,15 +1,78 @@
-const computeStudentsStats = (laboratoria) => {
-    //  console.log(datos)
-    let numEst = laboratoria.lima.generacion.cuarta.estudiantes;
-    
-    name.innerHTML = "";
-    
-    for (let i = 0; i < numEst.length; i++) {
-        name.innerHTML += `
-        <tr>
-        <th scope="col"> ${numEst[i].nombre}</th>
-        <th scope="col"> ${numEst[i].correo}</th>
-        </tr>
-        `
+window.computeStudentsStats = (laboratoria) => {
+
+    for (key in laboratoria){
+        let campus = key;
+        // console.log(campus);
+        const generations = Object.keys(laboratoria[key].generacion);
+        let generation;
+        let students;
+        generations.forEach((generation) => {
+            generation = generation;
+            students = laboratoria[key].generacion[generation].estudiantes
+            // console.log(students);
+        })
+        // let student;
+        // students.forEach((student) =>{
+        //     student = student;
+        //     console.log(student);
+        // })
+        name.innerHTML = "";  
+        for (let i = 0; i < students.length; i++) {
+            // listaEstudiantes.style.display = "block";
+            name.innerHTML += `
+            <tr>
+            <th scope="col"> ${students[i].nombre}</th>
+            <th scope="col"> ${students[i].correo}</th>
+
+            </tr>
+            `        
+        }
     }
+}
+
+
+
+
+
+// window.computeStudentsStats = (laboratoria) => {
+
+// }
+
+window.computeGenerationsStats = (laboratoria) => {
+    // const arrayResult = [];
+    
+    const obj = {
+        campus: '',
+        generation: '',
+        average: 0,
+        count: 0,
+    }
+    
+    for (key in laboratoria){       
+        //rellenando propiedad 'campus' con la key de Laboratoria
+        obj.campus = key;
+        
+        
+         //devolviendo un array del objeto de generaciones
+              
+        const generaciones = Object.keys(laboratoria[key].generacion);
+        // console.log(generaciones);
+        generaciones.forEach((generation) => {
+            obj.generation = generation;       
+             
+            const students = laboratoria[key].generacion[generation].estudiantes;            
+            for(student in students){
+                average = 0;
+                average += students[student].progreso.porcentajeCompletado;
+                // console.log(students.length);
+                average /= students.length;
+                obj.average = Math.round(average);
+                obj.count = students.length;
+            }
+            
+            console.log(obj);
+                        
+         })     
+    }
+    return obj
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,58 @@
-const cargarJSON = ()=>{
-    const json = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
-    fetch(json)
-        .then(res => res.json())
-        .then(data => {
-        console.log(data);
-        computeStudentsStats(data)          
-        })
- };
+const json = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
 
-  
-//interacción click estudiantes
-let clickEstudiantes = document.getElementById("estudiantes");
-clickEstudiantes.addEventListener("click", cargarJSON);
+const realoadJSON = () => {
+    fetch(json)
+    .then( response => response.json() )
+    .then((res) => {
+        computeStudentsStats(res);
+        computeGenerationsStats(res);
+        // drawCampus(generacionesFun);
+    })
+    // .catch((error) => {
+    //     console.log(error);
+    // });
+}
+
+
+
+//pintar tabla
+let listaEstudiantes = document.getElementById("tablaEstudiantes");
 
 //Variable que pintara la lista de estudiantes.
 let name = document.getElementById("lista_estudiantes");
- 
+
+//interacción click estudiantes
+let clickEstudiantes = document.getElementById("estudiantes");
+
+//evento para jalar data
+clickEstudiantes.addEventListener("click", realoadJSON);
+
+
+
+
+
+
+
+
+
+const drawCampus = (data) => {
+    // const sedes = Object.keys(data);
+
+    
+
+    // const containerCampus = document.getElementById('campus');
+
+/*    sedes.forEach((sede) => {
+        const option = document.createElement('a');
+        option.innerHTML = sede;
+        containerCampus.appendChild(option);
+    });
+
+    containerCampus.addEventListener('change', iteratorGenerations);*/
+};
+
+
+// const drawGenerations = (e) => {
+//     //computeGenerationsStats(laboratoria)
+// };
+
