4896a397b4487ad5fddf93c5c0049c526eeb1fb4
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-07-02 20:07:45 -0500

    Pintar Boton Lima

--- src/data.js
+++ src/data.js
@@ -1,25 +1,55 @@
-/*Código que tenga que ver con  funciones que  obtienen y manipulan los datos*/
 window.datadashboard = {
 
-computeStudentsStats: (laboratoria) => {
-fetch(`https://raw.githubusercontent.com/brenduchiz/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`)
-.then(respuesta => respuesta.json())
-.then ( data=> {
 
-tabla(data)
+computeStudentsStats: (laboratoria) => {
+
+fetch(`https://raw.githubusercontent.com/brenduchiz/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`)
+
+.then(respuesta => respuesta.json())
+.then (data=> {
+
+
+
+console.log(data)
+Estu(data)
+
 })
 
+const Estu = (data) => {
+let resultado ="";
+let estudiante = data.lima.generacion.tercera.estudiantes;
 
-function tabla(data) {
+for (let i = 0; i < estudiante.length; i++) {
 
-let estudiante = data.lima.generacion.cuarta.estudiantes;
-for (var i = 0; i < estudiante.length; i++) {
-let nombre_tercera=  estudiante[i].nombre;
-console.log(nombre_tercera);
-laboratoria.innerHTML = (nombre_tercera);
+let nombre_tercera=estudiante[i];
+let progreso =nombre_tercera.progreso;
+console.log(progreso.temas)
+
+resultado+=  `
+
+<div class="form-group">
+
+    <label for="exampleFormControlSelect2"></label>
+
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+
+      <option>Nombre: ${nombre_tercera.nombre}</option>
+      <option>Correo: ${nombre_tercera.correo}</option>
+      <option>Progreso</option>
+      <option>Duracion Programa: ${progreso.duracionPrograma}</option>
+      <option>Porcentaje Completado: ${progreso.porcentajeCompletado}</option>
+    </select>
+</div>
+
+`
+
+
+}
+laboratoria.innerHTML = resultado;
+return(resultado)
 }
 
 
 }
-}
+
 }
