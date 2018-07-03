a5a19305a2e3036c72332e9702aeb8c0f47f22b2
ViniciaLaraRangel <39544010+ViniciaLaraRangel@users.noreply.github.com>
2018-07-03 06:40:27 -0500

    cambios para pintar en pantalla srint dos

--- src/data.js
+++ src/data.js
@@ -1,46 +1,81 @@
-//Este es el código en FETCH, comenzar a enlazar con window data y main para cargar el JSON
 window.data = {
-    cargarData: () => {
-        let laboratoriaJSON = fetch('https://github.com/ViniciaLaraRangel/cdmx-2018-06-bc-core-am-data-dashboard/blob/master/data/laboratoria.json');
-//Hacer que la data sea tomada por las funciones
-        data.computeStudentsStats(laboratoriaJSON);
-//Usar coma para cerrar    
-    },
-//Crear la promesa para la primera función 
-    computeStudentsStats: (laboratoriaJSON) => {
-        laboratoria.then(function(res){
-            return res.json();
-    })
-        .then(function(data){
-            let sedes = Object.getOwnPropertyNames(data);
-            let generaciones = Object.values(data);
-            console.log(sedes);
-//Aquí acaba el primer nivel
-                generaciones.forEach(function(generacionesSeg){
-                    let generacionesInfo = Object.values(generacionesSeg.generacion);
-                    generacionesInfo.forEach(function(estudiantesSegundo){
-                        let estudiantesInfo = estudiantesSegundo.estudiantes;
-                        estudiantesInfo.forEach(function(avanceTercer){
-                            let porcentajeCompl = avanceTercer.progreso;
-                            let nombres = avanceTercer.nombre;
-                                console.log(nombres);
-                        })
-                })
-            })
-
-    })
-//Usar coma para cerrar
-},
-
-    computeGenerationsStats: () => {
-
+    cargandoDatos:() => {
+      let laboratoria = fetch('https://raw.githubusercontent.com/ViniciaLaraRangel/cdmx-2018-06-bc-core-am-data-dashboard/javascriptFetch/data/laboratoria.json');
+//Primera función para los test
+      data.computeStudentsStats(laboratoria);
     },
 
-    sortStudents: () => {  
+      computeStudentsStats: (laboratoria) => {
+              laboratoria.then(function(res){
+                  return res.json();
+              })
+              .then(function(data){
+                  let sedes = Object.getOwnPropertyNames(data);
+                  let generaciones = Object.values(data);
+                //Aquí hay que agregar el bucle que defina. PREGUNTAR AMALIA
+                  //console.log(sedes);
+                    //generaciones titulo
+                  generaciones.forEach(function (elements){
+                    let generation = Object.values(elements.generacion);
+//error
+                    generation.forEach(function (elements2){
+                      let estudiantes = elements2.estudiantes;
+                      estudiantes.forEach(function(elements3){
+                          //let infoEstudiantes =Object.values(elements3);
+                              let porcentajeCompl = elements3.progreso;
+                              let nombres = elements3.nombre;
+                                //console.log(nombres);
+                    })
 
-    }
+                    })
 
-    filterStudents: () => {
+                  })
+                  /*let obtenidos = data.lima.generacion.cuarta.estudiantes;
+                  //console.log(data.lima.generacion.cuarta.estudiantes)
+                  let contenedor = '';
+                  obtenidos.forEach(element => {
+                      contenedor += `<li>${element.nombre}</li>`;
+                      //console.log(element.nombre);
 
-    }
-};
+                  });
+                  let retornados = JSON.stringify(contenedor);
+                  let result =  document.getElementById("resultado");
+                  result.insertAdjacentHTML("beforeEnd", contenedor)
+                  //console.log(retornados);*/
+                  let obtenidos = data.lima.generacion.cuarta.estudiantes;
+                  let i = 1;
+                  let j = 1;
+                  obtenidos.forEach(element =>{
+                     document.getElementById(`nombre${i++}`).innerHTML = element.nombre;
+                     let students = Object.values(element.progreso);  
+                    document.getElementById(`porcentaje${j++}`).innerHTML = element.progreso.porcentajeCompletado;
+                  })
+              })
+      },
+
+      computeGenerationsStats: () => {
+
+      },
+
+      sortStudents: () => {
+
+      },
+
+      filterStudents: () => {
+
+      }
+
+   }
+   /*
+
+
+   fetch('../data/laboratoria.json')
+   .then(function(res){
+      return res.json();
+   })
+   .then(function(data){
+          let laboratoria = data.lima.generacion.cuarta.estudiantes;
+         let resultado = JSON.stringify(laboratoria);
+         console.log(resultado);
+          document.getElementById('resultado').innerHTML = resultado;
+   })*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-//Enlazar el JSON y las funciones de data.js con este manejo del DOM
-data.cargarData();
+let sede1 = document.getElementById('sedeTodas');
+let buton = document.getElementById('botonResultado');
+//buton.addEventListener('click', data.cargandoDatos());
+data.cargandoDatos();
+/*let sede = function(){
+     fetch(urlDirection.then(function(datos){
+     return datos.json();
+     }).then(function(data){
+     console.log(data.lima);
+   let sede1 = data.lima;
+ fullsede.innerHTML=sede1;
 
-//Declarar lo que se va a imprimir en el DOM
-let sede1 = document.getElementById("sede");
-let buton = document.getElementById("buton");
+});*/
