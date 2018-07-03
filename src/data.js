a7ffd8c4a472e44a59f12cdd2b3455e57c5279a4
Mónica Pavía <monicelica_pavi@hotmail.com>
2018-07-02 21:19:36 -0500

    Se logro que los datos pintados en menu fueran obtenidos desde el .json

--- src/data.js
+++ src/data.js
@@ -10,3 +10,65 @@ window.sortStudents = () => {
 window.filterStudents = () => {
 
 };
+
+window.computeStudentsStats = () =>{
+
+};
+window.computeGenerationsStats = () =>{
+
+};
+window.sortStudents = () => {
+
+};
+window.filterStudents = () => {
+
+};
+
+//url para obtener el json
+let url = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+
+//pintar datos en menu
+let sede = function(){
+    //ingresar la url
+  fetch(url).then(function(datos){
+    //decir q datos es un .json
+  return datos.json();
+  }).then(function(data){
+    //sedes se pinten en el munu a traves del getElementById
+    document.getElementById('lima').innerHTML = Object.keys(data)[0];
+    document.getElementById('mexico').innerHTML = Object.keys(data)[1];
+    document.getElementById('santiago').innerHTML = Object.keys(data)[2];
+    //pintando generaciones
+ 	generacionLima(Object.keys(data.lima.generacion));
+    generacionMexico(Object.keys(data.mexico.generacion));
+    generacionSantiago(Object.keys(data.santiago.generacion));
+    })
+  
+}
+
+
+
+let alumnas = function(){
+  fetch(url).then(function(datos){
+  return datos.json();
+  }).then(function(data){
+  //OBTENER NOMBRE
+  console.log(data.lima.generacion.cuarta.estudiantes[0].nombre);
+  let nombreLima = data.lima.generacion.cuarta.estudiantes[0].nombre;
+  nombre.innerHTML = nombreLima;
+  /*console.log(data.lima.generacion.cuarta.estudiantes.nombre);
+  let nombreLima = data.lima.generacion.cuarta.estudiantes.nombre;
+  console.log(Object.keys(nombreLima));
+  let allnombres = Object.keys(nombreLima);
+  console.log(allnombres[0]);
+  for(let i = 0; i<allnombres.length; i++) {
+    nombre.innerHTML+= "<div>" + allnombres[i] + "</div>";
+  }*/
+  //OBTENER CORREO
+  console.log(data.lima.generacion.cuarta.estudiantes[0].correo);
+   let correoLima = data.lima.generacion.cuarta.estudiantes[0].correo;
+    correo.innerHTML = correoLima;
+
+    })
+}
+  
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
 //url para obtener el json
-let url = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+//let url = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
 //crear let para poder pintar datos
 let showGeneraciones= document.querySelector("#sedes");
 let showSedes= document.querySelector("#generaciones");
@@ -10,13 +10,13 @@ let correo = document.querySelector("#correo");
 // a través del boton iniciar funcion
 
 //SEDES
-sedesAll.addEventListener("click", function(){
+check1.addEventListener("click", function(){
   sede(); 
 })
 //GENERACIONES
-sed.addEventListener("click", function(){
+/*sed.addEventListener("click", function(){
   generacion(); 
-})
+})*/
 
 //ALUMNAS
 
@@ -24,25 +24,50 @@ alumnasA.addEventListener("click", function(){
   alumnas(); 
 })
 
+//GENERACIONES LIMA
+const generacionLima = (allGeneraciones) => {
+  //id donde se creara un hijo para despues pintarlo
+  const generaciones = document.getElementById('generacionesLima');
+  allGeneraciones.forEach((generacion) => {
+  //se crea el label donde se pintara el hijo creado
+  const newLabel = document.createElement('label');
+  newLabel.innerHTML = generacion + ' generación';
+  generaciones.appendChild(newLabel);
+});
+}
 
-let sede = function(){
-  fetch(url).then(function(datos){
-  return datos.json();
-  }).then(function(data){
-  console.log(data);
-  let sedes= data;
-  console.log(Object.keys(sedes));
-  let allSedes = Object.keys(sedes);
-  console.log(allSedes[0]);
-  for(let i = 0; i<allSedes.length; i++) {
-        showSedes.innerHTML+= "<div>" + allSedes[i] + "</div>";
-    }
+//GENERACIONES MEXICO
+const generacionMexico = (allGeneraciones) => {
+  //id donde se creara un hijo para despues pintarlo
+  const generaciones = document.getElementById('generacionMexico');
+  allGeneraciones.forEach((generacion) => {
+  //se crea el label donde se pintara el hijo creado
+  const newLabel = document.createElement('label');
+  newLabel.innerHTML = generacion + ' generación';
+  generaciones.appendChild(newLabel);
+});
+}
 
-  })
+//GENERACIONES SANTIAGO
+const generacionSantiago = (allGeneraciones) => {
+  //id donde se creara un hijo para despues pintarlo
+  const generaciones = document.getElementById('generacionSantiago');
+  allGeneraciones.forEach((generacion) => {
+  //se crea el label donde se pintara el hijo creado
+  const newLabel = document.createElement('label');
+  newLabel.innerHTML = generacion + ' generación';
+  generaciones.appendChild(newLabel);
+});
 }
 
 
-let generacion = function(){
+
+
+
+
+
+
+/*let generacion = function(){
   fetch(url).then(function(datos){
   return datos.json();
   }).then(function(data){
@@ -56,31 +81,10 @@ let generacion = function(){
     }
 
   })
-}
+}*/
 
 
 
-let alumnas = function(){
-  fetch(url).then(function(datos){
-  return datos.json();
-  }).then(function(data){
-  //OBTENER NOMBRE
-  console.log(data.lima.generacion.cuarta.estudiantes[0].nombre);
-  let nombreLima = data.lima.generacion.cuarta.estudiantes[0].nombre;
-  nombre.innerHTML = nombreLima;
-  /*console.log(data.lima.generacion.cuarta.estudiantes.nombre);
-  let nombreLima = data.lima.generacion.cuarta.estudiantes.nombre;
-  console.log(Object.keys(nombreLima));
-  let allnombres = Object.keys(nombreLima);
-  console.log(allnombres[0]);
-  for(let i = 0; i<allnombres.length; i++) {
-    nombre.innerHTML+= "<div>" + allnombres[i] + "</div>";
-  }*/
-  //OBTENER CORREO
-  console.log(data.lima.generacion.cuarta.estudiantes[0].correo);
-   let correoLima = data.lima.generacion.cuarta.estudiantes[0].correo;
-    correo.innerHTML = correoLima;
-  
   /*console.log(Object.keys(sedeLimaGeneracion));
   let generacionesLimaAlumanas = Object.keys(sedeLimaGeneracion);
   console.log(generacionesLimaAlumanas[0]);
@@ -88,8 +92,7 @@ let alumnas = function(){
         showAlumnas.innerHTML+= "<div>" + generacionesLimaAlumanas[i] + "</div>";
     }*/
 
-  })
-}
+  
 
 
 
