71acb1b146a790dde419343cc57ecfaa1eead94d
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-07-05 13:27:36 -0500

    visualizacion

--- src/data.js
+++ src/data.js
@@ -1,7 +1,89 @@
-window.datadashboard = {
+/* funcion enla cual guardamos el valor de fetch*/
+window.getData = () => {
+    fetch(`https://raw.githubusercontent.com/brenduchiz/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`)
+    .then( response => response.json() )
+    .then((res) => {
+
+renderInfo(res)
+//const limaData=computeStudentsStats(resdata);
+
+//const mexData=computeGenerationsStats(resdata);
+//console.log(limaData)
 
 
-computeStudentsStats: (laboratoria) => {
+    })
+    .catch((error) => {
+
+    });
+}
+
+getData();
+
+
+
+window.computeStudentsStats = (laboratoria,genera,sede) => {
+let generac =genera;
+let sedelima= sede;
+console.log(sedelima)
+  let resultado ="";
+  let estudiante = laboratoria[sede].generacion[genera].estudiantes;
+
+
+  for (let i = 0; i < estudiante.length; i++) {
+  let nombre_tercera=estudiante[i];
+
+  let progreso =nombre_tercera.progreso;
+
+  resultado+=  `
+
+  <div class="form-group">
+
+      <label for="exampleFormControlSelect2"></label>
+
+      <select multiple class="form-control" id="exampleFormControlSelect2">
+
+        <option>Nombre: ${nombre_tercera.nombre}</option>
+        <option>Correo: ${nombre_tercera.correo}</option>
+        <option>Sede: ${sedelima}</option>
+        <option>Generacion:${generac}</option>
+
+        <option>Progreso</option>
+        <option>Duracion Programa: ${progreso.duracionPrograma}</option>
+        <option>Porcentaje Completado: ${progreso.porcentajeCompletado}</option>
+      </select>
+  </div>
+
+`
+}
+pintar_estudiantes.innerHTML = resultado;
+
+  };
+
+  window.computeGenerationsStats = (laboratoria) => {
+
+    };
+
+
+
+window.sortStudents = (laboratoria) => {
+
+
+};
+
+
+window.filterStudents = (laboratoria) => {
+
+
+};
+
+
+
+
+
+
+
+/*window.getdata = {
+
 
 fetch(`https://raw.githubusercontent.com/brenduchiz/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`)
 
@@ -14,7 +96,11 @@ console.log(data)
 Estu(data)
 
 })
+}*/
 
+/*
+computeStudentsStats: (laboratoria) => {
+}
 const Estu = (data) => {
 let resultado ="";
 let estudiante = data.lima.generacion.tercera.estudiantes;
@@ -50,6 +136,4 @@ return(resultado)
 }
 
 
-}
-
-}
+}*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 /*Código que tenga que ver con mostrar los datos en la pantalla*/
 
-let studentsimp=document.querySelector("#pintar_estudiantes");
+let pintcampus=document.querySelector("pintar_campus");
 let tercer=document.getElementById("lima_tercera");
 let cuarta=document.getElementById("lima_cuarta");
 let quinta=document.getElementById("lima_quinta");
@@ -10,10 +10,89 @@ let mquinta=document.getElementById("mex_quinta");
 let santercer=document.getElementById("sant_tercera");
 let sancuarta=document.getElementById("sant_cuarta");
 let sanquinta=document.getElementById("sant_quinta");
+let sedeLima="lima";
+let sedeMex="mexico";
+let sedeSant="santiago";
+renderInfo = (res) => {
+
+sedeLima="lima";
+sedeMex="mexico";
+sedeSant="santiago";
+
+/*Boton tercera Lima*/
+tercer.addEventListener("click",evento=>{
+let botonLima = evento.target.innerHTML;
+window.computeStudentsStats(res,botonLima,sedeLima)
+});
+
+/*Boton cuarta Lima*/
+cuarta.addEventListener("click",evento=>{
+let botonLima = evento.target.innerHTML;
+window.computeStudentsStats(res,botonLima,sedeLima)
+});
+
+
+/*Boton quinta Lima*/
+quinta.addEventListener("click",evento=>{
+let botonLima = evento.target.innerHTML;
+window.computeStudentsStats(res,botonLima,sedeLima)
+});
 
 
 
-tercer.addEventListener("click",evento=>{window.datadashboard.computeStudentsStats(studentsimp)});
+/*Boton tercera Mex*/
+mtercer.addEventListener("click",evento =>{
+let botonMex = evento.target.innerHTML;
+window.computeStudentsStats(res,botonMex,sedeMex)
+});
+
+/*Boton cuarta Mex*/
+mcuarta.addEventListener("click",evento=>{
+let botonMex = evento.target.innerHTML;
+window.computeStudentsStats(res,botonMex,sedeMex)
+});
+
+/*Boton quinta Mex*/
+mquinta.addEventListener("click",evento=>{
+let botonMex = evento.target.innerHTML;
+window.computeStudentsStats(res,botonMex,sedeMex)});
+
+
+
+/*Boton tercera San*/
+santercer.addEventListener("click",evento=>{
+let botonSan = evento.target.innerHTML;
+window.computeStudentsStats(res,botonSan,sedeSant)
+});
+
+/*Boton cuarta San*/
+sancuarta.addEventListener("click",evento=>{
+let botonSan = evento.target.innerHTML;
+window.computeStudentsStats(res,botonSan,sedeSant)
+});
+
+/*Boton quinta San*/
+sanquinta.addEventListener("click",evento=>{
+let botonSan = evento.target.innerHTML;
+window.computeStudentsStats(res,botonSan,sedeSant)
+});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*let nombre_tercera =
