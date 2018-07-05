0f8e0a99a30ba38aa205f5e4bab0a6ab87032316
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-07-05 12:33:39 -0500

    Sedes pintandose de forma adecuada, falta agregar funcion a botones

--- src/data.js
+++ src/data.js
@@ -1,16 +1,10 @@
+executeEvents = (data) =>{
+  let getData = document.getElementById("get");//Toma el valor del botón
+  getData.addEventListener("click", gettingVenue(data));//Evento Onclick para detonar las funciones de accion (declaradas en data.js) y las funciones de impresion
+}
+
 window.computeVenuesStats = (laboratoria) =>{
   let venuesArray =[];
-  let objVenues = {
-    venue: '',
-    //generation: '',
-    activeStudents : 0,
-    advanceStudents : 0,
-    inLowStudents : 0,
-    average : 0,
-    readingAverage: 0,
-    quizAverage: 0,
-    timeAverage : 0,
-  };
   let average = 0;
   let advanceStudents = 0;
   let inLowStudents =0;
@@ -18,21 +12,21 @@ window.computeVenuesStats = (laboratoria) =>{
   let readingAverage = 0;
   let quizAverage =0;
   let timeAverage =0;
-  for (key in laboratoria){
-    let myobj = new Object();
+  for (let key in laboratoria){
+    let myobj = {};
     average = 0;
     advanceStudents = 0;
     inLowStudents =0;
-    numStudents =0
+    numStudents =0;
     readingAverage = 0;
     quizAverage =0;
     timeAverage =0;
     myobj.venue = key;
     let generations = laboratoria[key].generacion;
-    for(generation in generations){
+    for(let generation in generations){
       const students = laboratoria[key].generacion[generation].estudiantes;
       numStudents += students.length;
-      for (student in students){
+      for (let student in students){
         average += students[student].progreso.porcentajeCompletado;
         timeAverage += students[student].progreso.duracionPrograma;
         if (students[student].progreso.porcentajeCompletado<=60){
@@ -41,10 +35,10 @@ window.computeVenuesStats = (laboratoria) =>{
           advanceStudents++;
         }
         const progresses = laboratoria[key].generacion[generation].estudiantes[student].progreso;
-        for (progres in progresses){
+        for (let progres in progresses){
           const items = laboratoria[key].generacion[generation].estudiantes[student].progreso[progres];
           //console.log(items);
-          for (item in items){
+          for (let item in items){
             const subs =Object.values(items);
           }
         }
@@ -63,47 +57,39 @@ window.computeVenuesStats = (laboratoria) =>{
      myobj.quizAverage = quizAverage;
      venuesArray.push(myobj);
   }
-    //venuesArray.push(objVenues);
-    console.log(venuesArray);
-    return (venuesArray);
+  console.log(venuesArray);
+  return (venuesArray);
 };
-  window.computeStudentsStats = (laboratoria) =>{
-    let studentArray =[];
-    let objStud ={
-      campus :'', generation :'', stats : [], average :0,
-      totalTime : 0, timeProm :0,
-    };
-    let averageS = 0;
-    let generationS = '';
-    let venuesS = [];
-    let statsS =[];
-    let totalTimeS = 0;
-    let timeProm =0;
-    let alerta ='';
-    let nameS = '';
-    let i =-1;
-    for (key in laboratoria){
-      let venuesL = Object.getOwnPropertyNames(laboratoria);
-      let generations = laboratoria[key].generacion;
-      i++;
-      for (generation in generations){
-        //let myStudent = new Object();
-        generationS = generation;
-        venuesS = venuesL[i];
-        //averageS =0; totalTimeS =0; timeProm =0; alerta ='';
-        const students= laboratoria[key].generacion[generation].estudiantes;
-        for (student in students){
-          let myStudent = new Object();
-          nameS = students[student].nombre;
-          averageS =0; totalTimeS =0; timeProm =0; alerta ='';
-          averageS = students[student].progreso.porcentajeCompletado;
-          if (students[student].progreso.porcentajeCompletado<=60){
-              alerta = 'Bajo avance';
-              }else if (students[student].progreso.porcentajeCompletado>=90) {
-                alerta = 'Alto avance';
-              }
-              else{ alerta = 'Medio avance';}
-          totalTimeS = students[student].progreso.duracionPrograma;
+window.computeStudentsStats = (laboratoria) =>{
+  let studentArray =[];
+  let averageS = 0;
+  let generationS = '';
+  let venuesS = [];
+  let totalTimeS = 0;
+  let timeProm =0;
+  let alerta ='';
+  let nameS = '';
+  let i =-1;
+  for (let key in laboratoria){
+    let venuesL = Object.getOwnPropertyNames(laboratoria);
+    let generations = laboratoria[key].generacion;
+    i++;
+    for (let generation in generations){
+      generationS = generation;
+      venuesS = venuesL[i];
+      const students= laboratoria[key].generacion[generation].estudiantes;
+      for (let student in students){
+        let myStudent = {};
+        nameS = students[student].nombre;
+        averageS =0; totalTimeS =0; timeProm =0; alerta ='';
+        averageS = students[student].progreso.porcentajeCompletado;
+        if (students[student].progreso.porcentajeCompletado<=60){
+          alerta = 'Bajo avance';
+        }else if (students[student].progreso.porcentajeCompletado>=90) {
+          alerta = 'Alto avance';
+        }
+        else{ alerta = 'Medio avance';}
+        totalTimeS = students[student].progreso.duracionPrograma;
         myStudent.campus = venuesS.toUpperCase();
         myStudent.generation = generationS.toUpperCase();
         myStudent.name = nameS;
@@ -117,176 +103,175 @@ window.computeVenuesStats = (laboratoria) =>{
     }
     console.log(studentArray);
     return (studentArray);
-  };
-  window.computeGenerationsStats = (laboratoria) =>{
-    let generationsArray =[];
-    let objGen = {
-      venue: '',
-      generation: '',
-      count : 0,
-      advanceStudents : 0,
-      inLowStudents : 0,
-      average : 0,
-      readingAverage: 0,
-      quizAverage: 0,
-      timeAverage : 0,
-    };
-    let averageO = 0;
-    let advanceStudentsO = 0;
-    let inLowStudentsO =0;
-    let countsO =0;
-    let readingAverageO = 0;
-    let quizAverageO =0;
-    let timeAverageO =0;
-    let venuesO ='';
-    let generationsO ='';
-    let i =-1;
-    for (key in laboratoria){
-      let generations = laboratoria[key].generacion;
-      let venuesValue = Object.getOwnPropertyNames(laboratoria);
-      i++;
-      for (generation in generations){
-        let myObj = new Object();
-        countsO =0; averageO = 0; advanceStudentsO = 0; inLowStudentsO =0; timeAverageO =0;
-        generationsO = generation;
-        venuesO = venuesValue[i];
-        const students = laboratoria[key].generacion[generation].estudiantes;
-        countsO += students.length;
-        for (student in students){
-          averageO += students[student].progreso.porcentajeCompletado;
-          timeAverageO += students[student].progreso.duracionPrograma;
-            if (students[student].progreso.porcentajeCompletado<=60){
-              inLowStudentsO++;
-              }else if (students[student].progreso.porcentajeCompletado>=90) {
+};
+window.computeGenerationsStats = (laboratoria) =>{
+  let generationsArray =[];
+  let averageO = 0;
+  let advanceStudentsO = 0;
+  let inLowStudentsO =0;
+  let countsO =0;
+  let readingAverageO = 0;
+  let quizAverageO =0;
+  let timeAverageO =0;
+  let venuesO ='';
+  let generationsO ='';
+  let i =-1;
+  for (key in laboratoria){
+    let generations = laboratoria[key].generacion;
+    let venuesValue = Object.getOwnPropertyNames(laboratoria);
+    i++;
+    for (generation in generations){
+      let myObj = {};
+      countsO =0; averageO = 0; advanceStudentsO = 0; inLowStudentsO =0; timeAverageO =0;
+      generationsO = generation;
+      venuesO = venuesValue[i];
+      const students = laboratoria[key].generacion[generation].estudiantes;
+      countsO += students.length;
+      for (student in students){
+        averageO += students[student].progreso.porcentajeCompletado;
+        timeAverageO += students[student].progreso.duracionPrograma;
+        if (students[student].progreso.porcentajeCompletado<=60){
+          inLowStudentsO++;
+        }else if (students[student].progreso.porcentajeCompletado>=90) {
           advanceStudentsO++;
-          }
         }
-        myObj.generation = generationsO;
-        myObj.venue = venuesO;
-        myObj.count =countsO;
-        myObj.average = Math.round(averageO / countsO);
-        myObj.timeAverage = Math.round(((timeAverageO / countsO) * (averageO / countsO))/100);
-        myObj.inLowStudents = inLowStudentsO;
-        myObj.advanceStudents = advanceStudentsO;
-        generationsArray.push(myObj);
       }
+      myObj.generation = generationsO;
+      myObj.venue = venuesO;
+      myObj.count =countsO;
+      myObj.average = Math.round(averageO / countsO);
+      myObj.timeAverage = Math.round(((timeAverageO / countsO) * (averageO / countsO))/100);
+      myObj.inLowStudents = inLowStudentsO;
+      myObj.advanceStudents = advanceStudentsO;
+      generationsArray.push(myObj);
     }
-    console.log(generationsArray);
-    return (generationsArray);
-  };
-  window.sortStudents = (students, orderBy, orderDirection)=>{
-    let ordered;
-     if (orderBy == 'name'){
-       if (orderDirection == 'ASC'){
-          ordered = students.sort(function(a,b){
-            var nameA = a.name.toLowerCase(), nameB=b.name.toLowerCase();
-            if (nameA<nameB) return -1;
-            if (nameA>nameB) return 1;
-            return 0;
-          })
-          console.log(ordered);
-       }else{
-         ordered = students.sort(function(a,b){
-           var nameA = a.name.toLowerCase(), nameB=b.name.toLowerCase();
-           if (nameA>nameB) return -1;
-           if (nameA<nameB) return 1;
-           return 0;
-         })
-         console.log(ordered);
-       }
-     }else if (orderBy == 'percentage'){
-         if (orderDirection == 'ASC'){
-            ordered = students.sort(function (a,b){
-              return (a.average - b.average);
-            });
-            console.log(ordered);
-         }else{
-         ordered = students.sort(function (a,b){
-           return (b.average - a.average);
-         });
-         console.log(ordered);
-       }
-     }
-     return (ordered);
-  };
-  window.filterStudents = (students, search) =>{
-    let filtered = [];
-    for (let i =0; i<students.length; i++){
-      var currentS = students[i];
-      if (currentS.name == search){
-        filtered.push(currentS);
-      }
+  }
+  console.log(generationsArray);
+  return (generationsArray);
+};
+window.sortStudents = (students, orderBy, orderDirection)=>{
+  let ordered;
+  if (orderBy == 'name'){
+    if (orderDirection == 'ASC'){
+      ordered = students.sort(function(a,b){
+        var nameA = a.name.toLowerCase(), nameB=b.name.toLowerCase();
+        if (nameA<nameB) return -1;
+        if (nameA>nameB) return 1;
+        return 0;
+      })
+    console.log(ordered);
+    }else{
+      ordered = students.sort(function(a,b){
+      var nameA = a.name.toLowerCase(), nameB=b.name.toLowerCase();
+      if (nameA>nameB) return -1;
+      if (nameA<nameB) return 1;
+      return 0;
+      })
+    console.log(ordered);
     }
-    console.log(filtered);
-    return (filtered);
-  };
-
-  window.deepStudentsStats = (laboratoria) =>{
-    let studentArray =[];
-    let objStud ={
-      campus :'', generation :'', stats : [], average :0,
-      totalTime : 0, timeProm :0,
-    };
-    let averageS = 0;
-    let generationS = '';
-    let venuesS = [];
-    let statsS =[];
-    let totalTimeS = 0;
-    let timeProm =0;
-    let alerta ='';
-    let nameS = '';
-    let emailS ='';
-    let i =-1;
-    for (key in laboratoria){
-      let venuesL = Object.getOwnPropertyNames(laboratoria);
-      let generations = laboratoria[key].generacion;
-      i++;
-      for (generation in generations){
-        //let myStudent = new Object();
-        generationS = generation;
-        venuesS = venuesL[i];
-        //averageS =0; totalTimeS =0; timeProm =0; alerta ='';
-        const students= laboratoria[key].generacion[generation].estudiantes;
+  }else if (orderBy == 'percentage'){
+    if (orderDirection == 'ASC'){
+      ordered = students.sort(function (a,b){
+      return (a.average - b.average);
+      });
+      console.log(ordered);
+    }else{
+      ordered = students.sort(function (a,b){
+      return (b.average - a.average);
+      });
+      console.log(ordered);
+    }
+  }
+  return (ordered);
+};
+window.filterStudents = (students, search) =>{
+  let filtered = [];
+  for (let i =0; i<students.length; i++){
+    var currentS = students[i];
+    if (currentS.name == search){filtered.push(currentS);}
+  }
+  console.log(filtered);
+  return (filtered);
+};
+window.deepStudentsStats = (laboratoria) =>{
+  let studentArray =[];
+  let averageS = 0;
+  let generationS = '';
+  let venuesS = [];
+  let statsS =[];
+  let totalTimeS = 0;
+  let timeProm =0;
+  let statusS ='';
+  let nameS = '';
+  let emailS ='';
+  let i =-1;
+  let completedPercentageS =[];
+  let percentageDurationS =[];
+  let itemS ='';
+  let j=0, k=0;
+  for (key in laboratoria){
+    let venuesL = Object.getOwnPropertyNames(laboratoria);
+    let generations = laboratoria[key].generacion;
+    i++;
+    for (generation in generations){
+      generationS = generation;
+      venuesS = venuesL[i];
+      const students= laboratoria[key].generacion[generation].estudiantes;
         for (student in students){
-          let myStudent = new Object();
+          let myStudent = {};
           nameS = students[student].nombre;
           emailS = students[student].correo;
-          averageS =0; totalTimeS =0; timeProm =0; alerta ='';
-          averageS = students[student].progreso.porcentajeCompletado;
-          if (students[student].progreso.porcentajeCompletado<=60){
-              alerta = 'Bajo avance';
-              }else if (students[student].progreso.porcentajeCompletado>=90) {
-                alerta = 'Alto avance';
-              }
-              else{ alerta = 'Medio avance';}
-          totalTimeS = students[student].progreso.duracionPrograma;
           const Stats= laboratoria[key].generacion[generation].estudiantes[student].progreso;
-          let stats = new Object();
-            stats.completedPercentage = Stats.porcentajeCompletado;
-            if (stats.completedPercentage<=60){
-                stats.status = 'Bajo avance';
-            }else if (stats.completedPercentage>=90) {
-                stats.status = 'Alto avance';
-            }else{ stats.status = 'Medio avance';}
-              const Topics = Stats.temas;
-              let topics = new Object();
-              topics.completedPercentage = Topics.porcentajeCompletado;
-              topics.percentageDuration = Topics.duracionTemaCompletado;
-              console.log(Topics);
-
-
-        myStudent.generation = generationS;
-        myStudent.campus = venuesS;
-        myStudent.average = averageS;
-        myStudent.advertisment = alerta;
-        myStudent.totalTime = totalTimeS;
-        myStudent.timeProm = (totalTimeS*averageS)/100;
-        myStudent.name = nameS;
-        myStudent.email = emailS;
-        studentArray.push(myStudent);
+          const arrayStats = Array.from(Object.values(Stats));
+          averageS =0; totalTimeS =0; timeProm =0; statusS =''; j=0;
+          let sts ={};
+          totalTimeS = arrayStats[j];
+          j++;
+          averageS = arrayStats[j];
+          if (averageS<=60){
+              statusS = 'Bajo avance';
+          }else if (averageS>=90) {
+              statusS = 'Alto avance';
+          }else{statusS = 'Medio avance';}
+          sts.completedPercentage = averageS;
+          sts.status = statusS;
+          j++;
+          const arrayTopic = Array.from(Object.values(arrayStats[j]));
+          const arrayUnitsNames = Array.from(Object.getOwnPropertyNames(arrayStats[j]));
+          arrayUnitsNames.forEach(function(unit){
+            let uni ={};
+            itemS = unit;
+            arrayTopic.forEach(function(top){
+              let tps={};
+              completedPercentageS = top.porcentajeCompletado;
+              percentageDurationS = top.duracionTemaCompletado;
+              tps.unit = itemS;
+              tps.completedPercentage = completedPercentageS;
+              tps.percentageDuration = percentageDurationS;
+              sts.topics = tps;
+            });
+          });
+          /*myStudent.stats.completedPercentage = Stats.porcentajeCompletado;
+          const units = Stats.temas;
+          const arrayUnits = Array.from(Object.values(units));
+          let topics = {};
+          arrayUnits.forEach(function(unite){
+            myStudent.stats.topics.completedPercentage = unite.porcentajeCompletado;
+            myStudent.stats.topics.percentageDuration = unite.duracionTemaCompletado;
+          });*/
+          myStudent.name = nameS;
+          myStudent.email = emailS;
+          myStudent.campus = venuesS;
+          myStudent.generation = generationS;
+          myStudent.timeProm = (totalTimeS*averageS)/100;
+          myStudent.totalTime = totalTimeS;
+          myStudent.stats = sts;
+          //myStudent.stats.completedPercentage = averageS;
+          //myStudent.stats.status = alerta;
+          studentArray.push(myStudent);
         }
       }
     }
-    console.log(studentArray);
-    return (studentArray);
-  };
+  console.log(studentArray);
+  return (studentArray);
+};
--- src/main.js
+++ src/main.js
@@ -1,104 +1,126 @@
+const laboratoria = "https://api.myjson.com/bins/1amyo6";//API con DATA a usar (base de datos de alumnas)
 
-const laboratoria = "https://api.myjson.com/bins/1amyo6";
-let goes= document.getElementById("go");
-//goes.addEventListener("click",go);
-go =()=>{
-  if (document.form.password.value=='tmmex2018' && document.form.login.value=='Lucile'){
-          document.form.submit();
-      }
-      else{
-           alert("Porfavor ingrese Usuario y Contraseña correctos.");
-      }
-  }
+  fetch(laboratoria)
+    .then(data => data.json())
+    .then((data) =>{
 
+      
 
-  let getData = document.getElementById("get");
-  let result = document.getElementById("result");
-
-  getData.addEventListener("click", gettingData);
-  function gettingData() {
-    result.innerHTML = gettinAPI(laboratoria);
-  }
-  /*gettinAPI genera acceso al obj de laboratoria.json que se encuentra en la API
-  por medio de FETCH*/
-    const gettinAPI = (laboratoria) => {
-      fetch(laboratoria)
-      .then(data => data.json())
-      .then((data) => {
-         //const venues = computeVenuesStats(data);
-         //const generations = computeGenerationsStats(data);
-         const students = computeStudentsStats(data);
-         //const orderBy ="percentage";
-         //const orderDirection = "DESC";
-         //const sort =sortStudents(students, orderBy, orderDirection);
-         const search = 'Aileen Edwyna';
-         const filter = filterStudents(students,search);
-         //const Students = deepStudentsStats(data);
-
-         //printGenerations(generations);
-         //printVenues(venues);
-         //printGenerations(generations);
-         //printStudents(students);
-         //printSort(sort);
-         printFilter(filter);
-      })
-      .catch((error) => {
-        console.log(error);
-        //Ve a pagina de error si no hay conexión
-        //window.location="../views/errorFiles/errorpopup.html";
-        console.log("Error");
+      executeEvents(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      //Ve a pagina de error si no hay conexión
+      //window.location="../views/errorFiles/errorpopup.html";
       });
-    }
-    //gettinAPI(laboratoria);
 
-  const printVenues = (venues) =>{
-    const sedesConteiner = document.getElementById('venues');
-    const sedes = (venues);
-    sedes.forEach((sede)=>{
-      const place = document.createElement('option');
-      place.innerHTML = Object.values(sede);
-      sedesConteiner.appendChild(place);
-    });
-  };
-  const printGenerations = (generations) =>{
-    const generacionesConteiner = document.getElementById('venues');
-    //const generaciones = Object.keys(generations);
-    const generaciones = (generations);
-    //console.log(generations);
-    generaciones.forEach((generacion)=>{
-      const place = document.createElement('option');
-      place.innerHTML = Object.values(generacion);
-      generacionesConteiner.appendChild(place);
-    });
-  };
-  const printStudents = (students) =>{
-    const estudiantesConteiner = document.getElementById('venues');
-    const estudiantes = (students);
-    estudiantes.forEach((estudiante)=>{
-      const place = document.createElement('option');
-      place.innerHTML = Object.values(estudiante);
-      estudiantesConteiner.appendChild(place);
-    });
-  };
-  const printSort = (sort)=>{
-    const sortConteiner = document.getElementById('venues');
-    const Sort = (sort);
-    Sort.forEach((so)=>{
-      const place = document.createElement('option');
-      place.innerHTML = Object.values(so);
-      sortConteiner.appendChild(place);
-    });
-  };
-  const printFilter = (filter)=>{
-    const cards = document.getElementsByClassName('card-data');
-    for (i = 0; i< cards.length && i < filter.length; i++)
-    {
-      cards[i].querySelector(".campus").innerHTML = filter[i].campus;
-      cards[i].querySelector(".generation").innerHTML = filter[i].generation;
-      cards[i].querySelector(".name").innerHTML = filter[i].name;
-      cards[i].querySelector(".average").innerHTML = filter[i].average;
-      cards[i].querySelector(".advertisment").innerHTML = filter[i].advertisment;
-      cards[i].querySelector(".totalTime").innerHTML = filter[i].totalTime;
-      cards[i].querySelector(".timeProm").innerHTML = filter[i].timeProm;
-    }
-  };
+gettingVenue=(data) =>{//Funcion a ejecutar si el evento Onclick se presenta
+  const venues = computeVenuesStats(data);
+  printVenues(venues);
+}
+//DECLARACION DE FUNCIONES PARA IMPRIMIR EN DOM
+const printVenues = (venues) =>{
+  const resultVen = document.getElementById('students-result');
+  let ven ='';
+  for(let i=0; i<venues.length; i++){
+    let venueAverage= Math.round(venues[i].average);
+    console.log(venues[i]);
+    ven += `<div class="col s3 m3">
+                <div class="card white darken-1">
+                  <div class="card-content card-data black-text">
+                  <span class="card-title" id="cards"></span>
+                  <h4>Sede: ${venues[i].venue}</h4>
+                  <h5>Prom. Completitud: ${venueAverage}${'%'}</h5>
+                  <p>Alumnas Activas: ${venues[i].activeStudents}</p>
+                  <p># Alumnas de bajo avance: ${venues[i].inLowStudents}</p>
+                  <p># Alumnas de alto avance: ${venues[i].advanceStudents}</p>
+                  </div>
+                </div>
+              </div>`
+  }
+  resultVen.innerHTML= ven;
+};
+const printGenerations = (generations) =>{
+  const resultGen = document.getElementById('students-result');
+  let gen ='';
+  for(let i=0; i<generations.length; i++){
+    gen += `<div class="col s3 m3">
+                <div class="card white darken-1">
+                  <div class="card-content card-data black-text">
+                  <span class="card-title" id="cards"></span>
+                  <h4>Generación: ${generations[i].generation}</h4>
+                  <h5>Sede: ${generations[i].venue}</h5>
+                  <p>Alumnas Activas: ${generations[i].count}</p>
+                  <p>Prom. Completitud: ${generations[i].average}${'%'}</p>
+                  <p>Prom. Tiempo invertido: ${generations[i].timeAverage}${'hrs'}</p>
+                  <p># Alumnas de bajo avance: ${generations[i].inLowStudents}</p>
+                  <p># Alumnas de alto avance: ${generations[i].advanceStudents}</p>
+                  </div>
+                </div>
+              </div>`
+  }
+  resultGen.innerHTML= gen;
+};
+const printStudents = (students) =>{
+  const resultStudents = document.getElementById('students-result');
+  let studentsS ='';
+  for(let i=0; i<students.length; i++){
+    studentsS += `<div class="col s3 m3">
+                <div class="card white darken-1">
+                  <div class="card-content card-data black-text">
+                  <span class="card-title" id="cards"></span>
+                  <h4">Sede: ${students[i].campus}</h4>
+                  <h5>Generacion: ${students[i].generation}</h5>
+                  <p>Nombre: ${students[i].name}</p>
+                  <p>Completitud: ${students[i].average}${'%'}</p>
+                  <p>Status: ${students[i].advertisment}</p>
+                  <p>Tiempo total del programa: ${students[i].totalTime}${'hrs'}</p>
+                  <p>Tiempo invertido: ${students[i].timeProm}${'hrs'}</p>
+                  </div>
+                </div>
+              </div>`
+  }
+  resultStudents.innerHTML= studentsS;
+};
+const printSort = (sort)=>{
+  const resultSort = document.getElementById('students-result');
+  let studentsSort ='';
+  for(let i=0; i<sort.length; i++){
+    studentsSort += `<div class="col s3 m3">
+                <div class="card white darken-1">
+                  <div class="card-content card-data black-text">
+                  <span class="card-title" id="cards"></span>
+                  <h4>Sede: ${sort[i].campus}</h4>
+                  <h5>Generacion: ${sort[i].generation}</h5>
+                  <p>Nombre: ${sort[i].name}</p>
+                  <p>Completitud: ${sort[i].average}${'%'}</p>
+                  <p>Status: ${sort[i].advertisment}</p>
+                  <p>Tiempo total del programa: ${sort[i].totalTime}${'hrs'}</p>
+                  <p>Tiempo invertido: ${sort[i].timeProm}${'hrs'}</p>
+                  </div>
+                </div>
+              </div>`
+  }
+  resultSort.innerHTML= studentsSort;
+};
+const printFilter = (filter)=>{
+  const resultFilter = document.getElementById('students-result');
+  let studentsFilter ='';
+  for(let i=0; i<filter.length; i++){
+    studentsFilter += `<div class="col s3 m3">
+                <div class="card white darken-1">
+                  <div class="card-content card-data black-text">
+                  <span class="card-title" id="cards"></span>
+                  <h4>Sede: ${filter[i].campus}</h4>
+                  <h5>Generacion: ${filter[i].generation}</h5>
+                  <p>Nombre: ${filter[i].name}</p>
+                  <p>Completitud: ${filter[i].average}${'%'}</p>
+                  <p>Status: ${filter[i].advertisment}</p>
+                  <p>Tiempo total del programa: ${filter[i].totalTime}${'hrs'}</p>
+                  <p>Tiempo invertido: ${filter[i].timeProm}${'hrs'}</p>
+                  </div>
+                </div>
+              </div>`
+  }
+  resultFilter.innerHTML= studentsFilter;
+};
