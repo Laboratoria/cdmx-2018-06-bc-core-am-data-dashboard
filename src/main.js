af405360ba811a666a3bf6ca0030a1b51d502c55
Sandra\Sandy <moreloscs200884@gmail.com>
2018-07-06 13:17:54 -0500

    Ultimos cambios de codigo

--- src/main.js
+++ src/main.js
@@ -19,6 +19,8 @@ for(let i=0; i<campus.length; i++){
 // Funcion que muestra datos de sedes y estudiantes  
 
   let sede = function(e){
+    // document.getElementById('btn').addEventListener('click', (event) => 
+    // let result ="";
     let campus= (e.target.value).toLowerCase();
       fetch(urlDirection).then(function(datos){
       return datos.json();
@@ -34,18 +36,58 @@ console.log(data);
 
 
     for(let i = 0; i<generaciones.length; i++) {
-        sedeLima.innerHTML+= "<div>" + generaciones[i] + "</div>";
+        sedeLima.innerHTML+= "<div>" +"Generacion: "+generaciones[i] + "</div>";//pinta sedes
         console.log(data[campus]);
             let est=data[campus].generacion[generaciones[i]].estudiantes
              
             for(let i = 0; i<est.length; i++) {
+              console.log(est)
+//               result += `<div class="card">
+//                             <div class="info">
+//                                 <p>Nombre: ${info[i].nombre}</p>
+//                                 <p>Correo: ${info[i].correo}</p>
+//                                 <p>Duracion: ${info[i].progreso.duracionPrograma}</p>
+//                                 <p>Progreso: ${info[i].progreso.porcentajeCompletado}</p>
+//                             </div>
+//                         </div>`
+//         }
+//         container.innerHTML = result;
+//     };
+// }
+
+
+
+// // renderInfo = (data) => {
+// //     document.getElementById('btn').addEventListener('click', (event) => {
+// //         let result = '';
+// //         console.log(Object.values(data)[0])
+// //         const info = Object.values(data)[0];
+
+// //         for (let i = 0; i < info.length; i++) {
+// //             console.log(info[i].progress.javascript)
+// //             result += `<div class="card">
+// //                             <div class="info">
+// //                                 <p>Nombre: ${info[i].name}</p>
+// //                                 <p>Active: ${info[i].active}</p>
+// //                                 <p>Progress Javascript: ${info[i].progress.javascript}</p>
+// //                                 <p>Progress UX: ${info[i].progress.ux}</p>
+// //                             </div>
+// //                         </div>`
+// //         }
+// //         container.innerHTML = result;
+// //     });
+// // }
+
+
 
            
         sedeLima.innerHTML+= "<div>" +"Nombre: "+est[i].nombre + "</div>";
         sedeLima.innerHTML+= "<div>" +"Correo: " +est[i].correo + "</div>";
-        sedeLima.innerHTML+= "<div>" +"Duracion programa: " +est[i].progreso.duracionPrograma+ "</div>";
-        sedeLima.innerHTML+= "<div>" +"Porcentaje completado: "+est[i].progreso.porcentajeCompletado+ "</div>";
+        sedeLima.innerHTML+= "<div>" +"Duracion programa: " +est[i].progreso.duracionPrograma+" hrs"+ "</div>";
+        sedeLima.innerHTML+= "<div>" +"Porcentaje completado: "+est[i].progreso.porcentajeCompletado+"%"+ "</div>";
         sedeLima.innerHTML+= "<div>" +"Turno: " +est[i].turno + "</div>";
+        console.log(est[i].progreso.porcentajeCompletado);
+
             }
           }
 //
