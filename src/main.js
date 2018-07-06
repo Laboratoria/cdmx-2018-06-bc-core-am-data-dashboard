203f300905ce01f1fc0a0536ba9b805c2c8faba4
CarisAvi21 <shinnytinystar@hotmail.com>
2018-07-06 13:17:20 -0500

    correo y turnos

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // usar este archivo para todo el código que tenga que ver con mostrar los datos en la pantalla
 //let urlDirection obtiene el json de la data desde git hub
 
-let urlDirection = 'https://raw.githubusercontent.com/ReikaMoss/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
+let urlDirection = 'https://api.myjson.com/bins/9n48i';
 let sedeLima = document.querySelector("#botonesSede");
 let sedeMexico= document.querySelector("#botonesSede1");
 let sedeSantiago = document.querySelector("#botonesSede2");
 let btn = document.querySelector("#lima");
 let btn1 = document.querySelector("#cdmx");
-let btn2 = document.querySelector("#cdmx2");
+let btn2 = document.querySelector("#santiago");
 
 // usando el boton de las sedes llamamos a la función para que aparezca en consola data>lima>generacion>cuarta
 
@@ -25,19 +25,44 @@ lima.addEventListener("click", function(){
       console.log(data.lima.generacion);
       let generaciones = Object.keys(sede);
 
-    for(let i = 0; i<generaciones.length; i++) {
+    for(let i = 0; i < generaciones.length; i++) {
         sedeLima.innerHTML+= "<div>" + generaciones[i] + "</div>";
         sedeLima.addEventListener("click",(e) =>{
           console.log(e.target.innerHTML);
         })
-        console.log(data.lima.generacion[generaciones[i]]);
+
+        let est = data.lima.generacion.cuarta.estudiantes
+
+           for(let i = 0; i < est.length; i++) {
+       sedeLima.innerHTML+= "<div>" + est[i].nombre + "</div>";
+       sedeLima.innerHTML+= "<div>" + est[i].correo + "</div>";
+       sedeLima.innerHTML+= "<div>" + est[i].turno + "</div>";
+           }
+
+        let est1 = data.lima.generacion.quinta.estudiantes
+
+           for(let i = 0; i < est1.length; i++) {
+       sedeLima.innerHTML+= "<div>" + est[i].nombre + "</div>";
+       sedeLima.innerHTML+= "<div>" + est[i].correo + "</div>";
+       sedeLima.innerHTML+= "<div>" + est[i].turno + "</div>";
+           }
+
+        let est2 = data.lima.generacion.tercera.estudiantes
+
+          for(let i = 0; i < est2.length; i++) {
+       sedeLima.innerHTML+= "<div>" + est[i].nombre + "</div>";
+       sedeLima.innerHTML+= "<div>" + est[i].correo + "</div>";
+       sedeLima.innerHTML+= "<div>" + est[i].turno + "</div>";
+           }
+
+        //console.log(data.lima.estudiantes[estudiantes[i]]);
             }
 
-console.log(data.lima.generacion.cuarta.estudiantes);
-console.log(data.lima.generacion.quinta.estudiantes);
+//console.log(data.lima.generacion.cuarta.estudiantes);
+//console.log(data.lima.generacion.quinta.estudiantes);
       })
+    }
 
-  }
   cdmx.addEventListener("click", function(){
     sede1();
   })
@@ -53,15 +78,15 @@ let sede1 = function(){
       console.log(generaciones[0]);
     for(let i = 0; i<generaciones.length; i++) {
         sedeMexico.innerHTML+= "<div>" + generaciones[i] + "</div>";
-
-    }
-console.log(data.mexico.generacion.tercera.estudiantes);
-console.log(data.mexico.generacion.cuarta.estudiantes);
-console.log(data.mexico.generacion.quinta.estudiantes);
+        sedeMexico.addEventListener("click",(e) =>{
+          console.log(e.target.innerHTML);
+        })
+        console.log(data.mexico.generacion[generaciones[i]]);
+            }
       })
 
    }
-  cdmx2.addEventListener("click", function(){
+  santiago.addEventListener("click", function(){
     sede2();
   })
 let sede2 = function(){
@@ -75,12 +100,11 @@ let sede2 = function(){
 
     for(let i = 0; i<generaciones.length; i++) {
         sedeSantiago.innerHTML+= "<div>" + generaciones[i] + "</div>";
-
-     }
-console.log(data.santiago.generacion.tercera.estudiantes);
-console.log(data.santiago.generacion.cuarta.estudiantes);
-console.log(data.santiago.generacion.quinta.estudiantes);
-
+        sedeSantiago.addEventListener("click",(e) =>{
+          console.log(e.target.innerHTML);
+        })
+        console.log(data.santiago.generacion[generaciones[i]]);
+            }
 
       })
 
