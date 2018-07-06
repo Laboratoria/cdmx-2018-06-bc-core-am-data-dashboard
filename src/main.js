0606be5fd013162d682e7573705daaaf1569b2e5
Sandra\Sandy <moreloscs200884@gmail.com>
2018-07-06 10:33:03 -0500

    Cambios codigo JS

--- src/main.js
+++ src/main.js
@@ -16,14 +16,14 @@ for(let i=0; i<campus.length; i++){
 
 
 }
-// Funcion que muestra datos de sedes y alumnas de lima en consolg
+// Funcion que muestra datos de sedes y estudiantes  
 
   let sede = function(e){
     let campus= (e.target.value).toLowerCase();
       fetch(urlDirection).then(function(datos){
       return datos.json();
       }).then(function(data){
-
+console.log(data);
 
        
       let gen = data[campus].generacion;
@@ -39,13 +39,13 @@ for(let i=0; i<campus.length; i++){
             let est=data[campus].generacion[generaciones[i]].estudiantes
              
             for(let i = 0; i<est.length; i++) {
-              
+
            
-        sedeLima.innerHTML+= "<div>" + est[i].nombre + "</div>";
-        sedeLima.innerHTML+= "<div>" + est[i].correo + "</div>";
-        sedeLima.innerHTML+= "<div>" + est[i].progreso.duracionPrograma+ "</div>";
-        sedeLima.innerHTML+= "<div>" + est[i].progreso.porcentajeCompletado+ "</div>";
-        sedeLima.innerHTML+= "<div>" + est[i].turno + "</div>";
+        sedeLima.innerHTML+= "<div>" +"Nombre: "+est[i].nombre + "</div>";
+        sedeLima.innerHTML+= "<div>" +"Correo: " +est[i].correo + "</div>";
+        sedeLima.innerHTML+= "<div>" +"Duracion programa: " +est[i].progreso.duracionPrograma+ "</div>";
+        sedeLima.innerHTML+= "<div>" +"Porcentaje completado: "+est[i].progreso.porcentajeCompletado+ "</div>";
+        sedeLima.innerHTML+= "<div>" +"Turno: " +est[i].turno + "</div>";
             }
           }
 //
@@ -56,58 +56,4 @@ for(let i=0; i<campus.length; i++){
       })
 
   }
-  cdmx.addEventListener("click", function(){
-    sede1();
-  })
-let sede1 = function(){
-      fetch(urlDirection).then(function(datos){
-      return datos.json();
-      }).then(function(data){
-      console.log(data.mexico.generacion);
-      let sede1 = data.mexico.generacion;
-      console.log(Object.keys(sede1));
-      let generaciones = Object.keys(sede1);
-
-      console.log(generaciones[0]);
-    for(let i = 0; i<generaciones.length; i++) {
-        sedeMexico.innerHTML+= "<div>" + generaciones[i] + "</div>";
-
-    }
-console.log(data.mexico.generacion.tercera.estudiantes);
-console.log(data.mexico.generacion.cuarta.estudiantes);
-console.log(data.mexico.generacion.quinta.estudiantes);
-console.log(data.mexico.generacion.tercera.estudiantes[0].correo);
-console.log(data.mexico.generacion.tercera.estudiantes[0].nombre);
-console.log(data.mexico.generacion.tercera.estudiantes[0].progreso);
-console.log(data.mexico.generacion.tercera.estudiantes[0].turno);
-      })
-
-   }
-  cdmx2.addEventListener("click", function(){
-    sede2();
-  })
-let sede2 = function(){
-      fetch(urlDirection).then(function(datos){
-      return datos.json();
-      }).then(function(data){
-      console.log(data.santiago.generacion);
-      let sede2 = data.santiago.generacion;
-      console.log(Object.keys(sede2));
-      let generaciones = Object.keys(sede2);
-
-    for(let i = 0; i<generaciones.length; i++) {
-        sedeSantiago.innerHTML+= "<div>" + generaciones[i] + "</div>";
-
-     }
-console.log(data.santiago.generacion.tercera.estudiantes);
-console.log(data.santiago.generacion.cuarta.estudiantes);
-console.log(data.santiago.generacion.quinta.estudiantes);
-console.log(data.santiago.generacion.tercera.estudiantes[0].correo);
-console.log(data.santiago.generacion.tercera.estudiantes[0].nombre);
-console.log(data.santiago.generacion.tercera.estudiantes[0].progreso);
-console.log(data.santiago.generacion.tercera.estudiantes[0].turno);
-
-
-      })
-
-  }
+  
