0d92a018649327088e862d265fad172a3f2a89e6
emeraldng <ifglitterthen@gmail.com>
2018-07-07 12:55:42 -0500

    Agregado comentarios en el data.js y main.js

--- src/data.js
+++ src/data.js
@@ -1,18 +1,52 @@
-const url = 'https://raw.githubusercontent.com/emeraldng/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
 
-window.onload = () => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            //esta func aun no existe, aqui van tantas fun necesites con la misma info
-            renderInfo(data);
-
-        })
-        .catch(error => {
-            console.log('errormio');
-
-        })
-
+window.computeStudentsStats = (laboratoria) => {
+    const students = [];
+    const obj = {
+        campus: '',
+        generation: '',
+    };
+    
 
 }
+
+window.computeGenerationsStats = (laboratoria) => {
+    const generationsArray = [];
+    const obj = {
+        campus: '',
+        generation: '',
+        average: 0,
+        count: 0,
+    };
+    let average = 0;
+    for (key in laboratoria) {
+        obj.campus = key;
+        average = 0;
+        //aqui la solucion a la vida, eso de retornar una fun que declaraste en el window y la usaras para dibujar.
+        const generations = Object.keys(laboratoria[key].generacion);
+        generations.forEach((generation) => {
+            obj.generation = generation;
+            const students = laboratoria[key].generacion[generation].estudiantes;
+            for (student in students) {
+                average += students[student].progreso.porcentajeCompletado;
+                average = average / students.length;
+                obj.average = average;
+                obj.count = students.length;
+                generationsArray.push(obj);
+
+            }
+        })
+
+    }
+    return generationsArray;
+}
+
+
+window.sortStudents = (laboratoria) => {
+
+}
+
+window.filterStudents = (laboratoria) => {
+
+}
+
+  
--- src/main.js
+++ src/main.js
@@ -1,37 +1,21 @@
-//const porque nunca cambia el div, solo la info depositada
-const container = document.getElementById('result');
+const url = 'http://api.myjson.com/bins/1efx86';
 
-const renderInfo = (data) => {
-    document.getElementById('btn').addEventListener('click', (event) => {
-        let result = '';
-        console.log(Object.keys(data.lima.generacion))
-        const info = Object.keys(data.lima.generacion);
-        console.log(Object.keys(info));
-
-        for (var generacion in info) {
-            if (info.hasOwnProperty(generacion)) {
-                console.log("la clave es " + generacion + " y el valor es " + info[generacion])
-                //console.log(info.lima.generacion)
-
-                let arrLaboratoriaEstudiantes = data.lima.generacion[info[generacion]].estudiantes;
-
-                for (i = 0; i < arrLaboratoriaEstudiantes.length; i++) {
-                    result += `<div class='card'>
-                                <div class='info'>
-                                   <p>Nombre: ${data.lima.generacion[info[generacion]].estudiantes[i].nombre}</p>
-                                   <p>"01-Introduccion-a-programacion" - Duracion: ${data.lima.generacion[info[generacion]].estudiantes[i].progreso.temas["01-Introduccion-a-programacion"].duracionTema}</p>
-                                    </div>
-                                    </div>`
-
-
-                }
-            }
-            container.innerHTML = result;
-
-        }
+//la data se carga el reload la pagina
+window.onload = () => {
+    fetch(url)
+    .then( response => response.json() )
+    .then((data) => {
+        //aquí van toda las funciones que ultilizaran la data (las de window y las de dibujar)
+        console.log(data)
+        computeStudentsStats(data);
+        computeGenerationsStats(data)
+        sortStudents(data)
+        filterStudents(data)
+    })
+    .catch((error) => {
+        console.log(error);
     });
 }
 
-document.getElementById('delete').addEventListener('click', (event) => {
-    container.innerHTML = '';
-})
+
+
