faa787368c3a85fb4da62cb3a1868377ed8f10d4
MiriamGaGu <mgg_pjl@yahoo.com>
2018-07-09 18:55:47 -0500

    Neew index con tabs

--- src/main.js
+++ src/main.js
@@ -1,39 +1,19 @@
-let api = "https://api.myjson.com/bins/1efx86"
+$(".dropdown-trigger").dropdown();
+const url = 'https://raw.githubusercontent.com/emeraldng/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
 
-//const porque nunca cambia el div, solo la info depositada
-const container = document.getElementById('result');
+window.onload = () => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            //esta func aun no existe, aqui van tantas fun necesites con la misma info
+            //renderInfo(data);
+            //console.log(renderInfo);
+        })
+        .catch(error => {
+            console.log('errormio');
 
-const renderInfo = (data) => {
-    document.getElementById('btn Lima').addEventListener('click', (event) => {
-        let result = '';
-        console.log(Object.keys(data.lima.generacion))
-        const info = Object.keys(data.lima.generacion);
-        console.log(Object.keys(info));
-
-        for (var generacion in info) {
-            if (info.hasOwnProperty(generacion)) {
-                console.log("la clave es " + generacion + " y el valor es " + info[generacion])
-                    //console.log(info.lima.generacion)
-
-                let arrLaboratoriaEstudiantes = data.lima.generacion[info[generacion]].estudiantes;
-
-                for (i = 0; i < arrLaboratoriaEstudiantes.length; i++) {
-                    result += `<div class='card'>
-                                <div class='info'>
-                                   <p>Nombre: ${data.lima.generacion[info[generacion]].estudiantes[i].nombre}</p>
-                                   <p>"01-Introduccion-a-programacion" - Duracion: ${data.lima.generacion[info[generacion]].estudiantes[i].progreso.temas["01-Introduccion-a-programacion"].duracionTema}</p>
-                                    </div>
-                                    </div>`
+        })
 
 
-                }
-            }
-            container.innerHTML = result;
-
-        }
-    });
 }
-
-document.getElementById('delete').addEventListener('click', (event) => {
-    container.innerHTML = '';
-})
