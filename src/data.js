f3fde0a8f2246476fdefd3eb101ae86da705cc69
MicheleBecerra <michbecerra@hotmail.com>
2018-07-09 02:19:41 -0500

    Se llego a los subtemas en la funcion 1

--- src/data.js
+++ src/data.js
@@ -1,4 +1,3 @@
-
 console.log("conectado");
 
 const showResult = document.getElementById("result-list");
@@ -12,44 +11,76 @@ window.computeStudentsStats = (laboratoria) => {
         let name = "";
         let email = "";
         let turn = "";
+        let completedPercentage = "";
+        let percentDuration = "";
+        let stats = {};
+        let topics = {};
         
-        let stats = {
-            status: "",
-        };
-    
-
     for (findSede in laboratoria) {
         campus = findSede;
         const generations = Object.keys(laboratoria[findSede].generacion);
-        for (let i = 0; i < generations.length; i++) {
-           // const findGeneration = Object.keys(laboratoria[findSede].generacion[generations[i]]);
-            generation = generations[i];
-            let generationLevel = generations[i];
-            //console.log(student);
-            let studentsLength = laboratoria[findSede].generacion[generationLevel].estudiantes;
-            for (let i = 0; i < studentsLength.length; i++) {
-                name = studentsLength[i].nombre;
-                email = studentsLength[i].correo;
-            
-                student.push({"name": name, "email": email, "campus": campus, "generation": generation, "stats": stats});
-                
-            }
-            //console.log(student); 
+        generations.forEach((eachGeneration) => {
+        generationLevel = eachGeneration ;
+        //console.log("2.2", generationLevel);
+        const studentsLength = laboratoria[findSede].generacion[generationLevel].estudiantes;
+        studentsLength.forEach((students) => {            
+        name = students.nombre;
+        email = students.correo;
+        turn = students.turno ; 
+        completedPercentage = students.progreso.porcentajeCompletado;
+        status = completedPercentage;
+        if (status <= 60){
+            studentPerformance = "Rendimiento por debajo del 60%.";
+        }else if (status > 90 ) {
+            studentPerformance = "Rendimiento, por arriba del 90%.";
+        } else {
+            studentPerformance = "Rendimiento dentro del promedio.";
+     };
+       const  studentsTopics = students.progreso.temas;
+        for(topic in studentsTopics) {
+        completedPercentage1 = studentsTopics[topic].porcentajeCompletado;
+        let durationTheme = studentsTopics[topic].duracionTema;
+        completedTheme = studentsTopics[topic].duracionTemaCompletado;
+        percentageDuration = parseInt((completedTheme * 100) / durationTheme );
+        //console.log("8", percentageDuration );
+
+        
+        const subTopics = studentsTopics[topic].subtemas;
+        for (subItem in subTopics){
+        type = subTopics[subItem].tipo;
+        subItemDuration = subTopics[subItem].duracionSubtema;
         }
-        console.log(student); 
+        const  totalSubTopics = subTopics[subItem].subtemasTotales; 
+        const completedTopics = subTopics[subItem].subtemasCompletados; 
+        completedPercentage2 = parseInt((completedTopics * 100 )/ totalSubTopics )
     }
-};
-
-window.computeGenerationsStats = (laboratoria) => {
-
-
-};
-
-window.sortStudents = (students, orderBy, orderDirection) => {
-
-};
-
-window.filterStudents = (students, search) => {
-
+        
+        student.push({
+        "name": name, 
+        "email": email, 
+        "campus": campus, 
+        "generation": generationLevel, 
+        "turn": turn, 
+        "stats": { 
+            "status": studentPerformance , 
+            "completedPercentage":completedPercentage, 
+            "topics": { 
+                "topic":{  
+                    "completedPercentage": completedPercentage1, 
+                    "percentageDuration":percentageDuration,
+                    "subtopics": {
+                        "completedPercentage": completedPercentage2,
+                        "duration": subItemDuration ,
+                        "type": type
+                    }
+                    }
+        }
+        }
+        }); 
+    });   
+       
+        });
+    }
+     console.log("1.1", student);
 }
-
+ 
