3f455038e1f8db51e980ed4cc50c3ff23dfcfacd
soberanesmajo <soberanesmajo@gmail.com>
2018-07-09 10:07:15 -0500

    bajando cambios de botones (sedes/generaciones)

--- src/data.js
+++ src/data.js
@@ -1,69 +1,66 @@
 // LLAMANDO JSON        
 
-const json = "https://raw.githubusercontent.com/soberanesmajo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+const json = 'https://raw.githubusercontent.com/soberanesmajo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
 
 const gettingData = (data) => {
-  //Mando a llamar a json mediante fetch.
+  // Mando a llamar a json mediante fetch.
   fetch(data)
-    .then((res) => { //Aquí ya tiene la información.
-      return res.json(); //Entonces le digo que a esa información la retorne como un archivo json.
+    .then((res) => { // Aquí ya tiene la información.
+      return res.json(); // Entonces le digo que a esa información la retorne como un archivo json.
     })
-    .then((laboratoria) => { //Aquí le asigno el nombre de laboratoria a la información que envió.
+    .then((laboratoria) => { // Aquí le asigno el nombre de laboratoria a la información que envió.
       sedes(laboratoria);
-      generations(laboratoria);
-      computeGenerationsStats(laboratoria); //Aquí estoy diciendo que a mis siguienten funciones, les estoy mandando esa data.
+      // generations(laboratoria);
+      computeGenerationsStats(laboratoria); // Aquí estoy diciendo que a mis siguienten funciones, les estoy mandando esa data.
       computeStudentsStats(laboratoria);
-
     })
     .catch((error) => {
       console.log(error);
-      //Aquí va mi función para un posible error.
-    })
-}
+      // Aquí va mi función para un posible error.
+    });
+};
 gettingData(json);
 
 window.computeGenerationsStats = (laboratoria) => {
- 
   let generation = [];
 
-  let generation1 = "";
-  let sede = "";
+  let generation1 = '';
+  let sede = '';
   let average1 = 0;
   let count1 = 0;
 
   for (key in laboratoria) {
     sede = key; // Está me arroja las llaves del objeto JSON (lima, mexico, santiago)
-    generation1 = Object.keys(laboratoria[sede].generacion); //Indico que quiero entrar a generación. Object.keys convierte a array mi objeto y marco la ruta a generaciones. Siempre indico la ruta antes de iterar.
+    generation1 = Object.keys(laboratoria[sede].generacion); // Indico que quiero entrar a generación. Object.keys convierte a array mi objeto y marco la ruta a generaciones. Siempre indico la ruta antes de iterar.
   
-    generation1.forEach(elements => { //generation1 es mi arreglo a iterar. forEach me regresa los elementos de ese array.
+    generation1.forEach(elements => { // generation1 es mi arreglo a iterar. forEach me regresa los elementos de ese array.
       let average2 = 0; // Esta variable hace el corte del flujo por generación.
       generation1 = elements; // generation1 es igual a elements porque son las propiedades específicas (cuarta, quinta, tercera).
     
-      const students = laboratoria[sede].generacion[generation1].estudiantes; //Se usa [] para entrar porque es variable. Sino es con .
+      const students = laboratoria[sede].generacion[generation1].estudiantes; // Se usa [] para entrar porque es variable. Sino es con .
       for (student in students) {
-        average2 += students[student].progreso.porcentajeCompletado; //Llegando al porcentajeCompletado (un número) se estará sumando.
-        average1 = Math.round(average2 / students.length); //Saca promedio y redondea la cifra.
+        average2 += students[student].progreso.porcentajeCompletado; // Llegando al porcentajeCompletado (un número) se estará sumando.
+        average1 = Math.round(average2 / students.length); // Saca promedio y redondea la cifra.
         count1 = students.length;
       }
       generation.push({
-        "generation": generation1,
-        "campus": sede,
-        "average": average1,
-        "count": count1
+        'generation': generation1,
+        'campus': sede,
+        'average': average1,
+        'count': count1
       });
-    })
+    });
   }
-  return generation
-}
+  return generation;
+};
 
 window.computeStudentsStats = (laboratoria) => {
-
   let student = [];
 
-  let nameStudent = "";
-  let emailStudent = "";
-  let sede = "";
-  let generation1 = "";
+  let nameStudent = '';
+  let emailStudent = '';
+  let sede = '';
+  let generation1 = '';
   let stats = {};
   let status = 0;
   let completedPercentage1 = 0;
@@ -72,7 +69,7 @@ window.computeStudentsStats = (laboratoria) => {
   let percentageDuration2 = 0;
   let subtopics = {};
   let completedPercentage3 = 0;
-  let type1 = "";
+  let type1 = '';
   let duration1 = 0;
 
   for (key in laboratoria) {
@@ -117,36 +114,35 @@ window.computeStudentsStats = (laboratoria) => {
         }
 
         student.push({
-          "name": nameStudent,
-          "email": emailStudent,
-          "campus": sede,
-          "generation": generation1,
-          "stats": {
-            "status": status,
-            "completedPercentage": completedPercentage1,
-            "topics": {
-              "completedPercentage": completedPercentage2,
-              "percentageDuration": percentageDuration2,
-              "subtopics": {
-                "completedPercentage": completedPercentage3,
-                "type": type1,
-                "duration": duration1
+          'name': nameStudent,
+          'email': emailStudent,
+          'campus': sede,
+          'generation': generation1,
+          'stats': {
+            'status': status,
+            'completedPercentage': completedPercentage1,
+            'topics': {
+              'completedPercentage': completedPercentage2,
+              'percentageDuration': percentageDuration2,
+              'subtopics': {
+                'completedPercentage': completedPercentage3,
+                'type': type1,
+                'duration': duration1
               }
             }
           }
         });
       }
-    })
+    });
   }
   console.log(student);
   return student;
-  
-}
+};
 
-window.sortStudents = ( students,orderBy,orderDirection ) => {
+window.sortStudents = (students, orderBy, orderDirection) => {
 
-}
+};
 
-window.filterStudents = (students,search) => {
+window.filterStudents = (students, search) => {
 
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,87 +1,70 @@
-let sede = document.getElementById("buttons");
-let ingresar = document.getElementById("ingresar");
-let inputName = document.getElementById("input-name");
-let password = document.getElementById("input-password");
-let form = document.getElementById("form");
-let accessMexico = document.getElementById("mexico");
-let accesLima = document.getElementById("lima");
-let accessSantiago = document.getElementById("santiago");
+let sede = document.getElementById('buttons-campus');
+let gen = document.getElementById('buttons-generations');
+let ingresar = document.getElementById('ingresar');
+let inputName = document.getElementById('input-name');
+let password = document.getElementById('input-password');
+let form = document.getElementById('form');
+// let accessMexico = document.getElementById("mexico");
+// let accesLima = document.getElementById("lima");
+// let accessSantiago = document.getElementById("santiago");
 
 // Ocultar las sedes
-sede.style.display="none";
+sede.style.display = 'none';
+gen.style.display = 'none';
 
 // Botones de login
-ingresar.addEventListener("click", obtenerDatos = ()=> { 
-    let usuario1 = inputName.value;
-    let password1 = password.value;
-    
-        if (usuario1==="lucile" && password1==="123"){
-        sede.style.display="block";
-        form.style.display="none";
+ingresar.addEventListener('click', obtenerDatos = () => {
+  let usuario1 = inputName.value;
+  let password1 = password.value;
 
-    }else{
-        alert("Usuario no identificado");
-    }
+  if (usuario1 === 'lucile' && password1 === '123') {
+    sede.style.display = 'block';
+    form.style.display = 'none';
+  } else {
+    alert('Usuario no identificado');
+  }
 });
 
 // Función para pintar sedes en el DOM
 const sedes = (laboratoria) => {
-    const containerButtons= document.getElementById("container-buttons");
-    for(key in laboratoria){
-        const divCol = document.createElement("div");
-        const a= document.createElement("a");
-        divCol.classList.add("col-4");
-        a.id=key;
-        a.innerHTML=key;
-        divCol.appendChild(a); 
-        // falta agregar evento al divCol para que ese button detone una funcion
-        containerButtons.appendChild(divCol);
-        // console.log(key);
+  const containerButtonsCampus = document.getElementById('container-buttons-campus');
+  for (key in laboratoria) {
+    const divCol = document.createElement('div');
+    const aElement = document.createElement('a');
+    divCol.classList.add('col-4');
+    aElement.id = key;
+    aElement.innerHTML = key;
+    divCol.appendChild(a);
+    containerButtonsCampus.appendChild(divCol);
+
+    divCol.addEventListener('click', () => generations(laboratoria));
+  }
+};
+
+const generations = (laboratoria) => {
+  sede.style.display = 'none';
+  gen.style.display = 'block';
+  const selectSede = event.target.id;
+  const containerButtonsGenerations = document.getElementById('container-buttons-generations');
+  for (key in laboratoria) {
+    if (key === selectSede) {
+      generation1 = Object.keys(laboratoria[key].generacion);
+      generation1.forEach(elements => {
+        generation1 = elements;
+
+        const divCol = document.createElement('div');
+        const aElement = document.createElement('a');
+        divCol.classList.add('col-4');
+        aElement.id = generation1;
+        aElement.innerHTML = generation1;
+        divCol.appendChild(a);
+        containerButtonsGenerations.appendChild(divCol);
+      });
     }
-}
-const generations = (laboratoria) => {
-    const containerButtons= document.getElementById("container-buttons");
-    for(key in laboratoria){
-        generation1 = Object.keys(laboratoria[key].generacion); //Indico que quiero entrar a generación. Object.keys convierte a array mi objeto y marco la ruta a generaciones. Siempre indico la ruta antes de iterar.
-        generation1.forEach(elements => { //generation1 es mi arreglo a iterar. forEach me regresa los elementos de ese array.
-        generation1 = elements; // generation1 es igual a elements porque son las propiedades específicas (cuarta, quinta, tercera).
-        
-        const divCol = document.createElement("div");
-        const a= document.createElement("a");
-        divCol.classList.add("col-4");
-        a.id=generation1;
-        a.innerHTML=generation1;
-        divCol.appendChild(a); 
-        // falta agregar evento al divCol para que ese button detone una funcion
-        containerButtons.appendChild(divCol);
-        // console.log(key);
-    })
-}
-}
-// const generations = (laboratoria ) => {
-//     const containerButtonsGenerations = document.getElementById("container-buttons-generations");
+  }
+};
 
-
-const generations = (laboratoria) => {
-    const containerButtons = document.getElementById("container-buttons");
-    for (key in laboratoria) {
-        generation1 = Object.keys(laboratoria[key].generacion); 
-        generation1.forEach(elements => { 
-        generation1 = elements; 
-        const divCol = document.createElement("div");
-        const a= document.createElement("a");
-        divCol.classList.add("col-4");
-        a.id=generation1;
-        a.innerHTML=generation1;
-        divCol.appendChild(a); 
-        // falta agregar evento al divCol para que ese button detone una funcion
-        containerButtons.appendChild(divCol);
-        // console.log(key);
-        }
-        )}
-}
-
-//Botones de ingreso a generaciones
+// Botones de ingreso a generaciones
 // accessMexico.addEventListener("click", event => {
 //     let access = event.target.id;
 //     gettingData(access);
