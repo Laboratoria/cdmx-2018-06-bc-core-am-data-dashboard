c8be87d06f15a9a5082060b7e7945aacf35a990c
AuraMelisa01 <auramelisa6@gmail.com>
2018-07-09 10:40:22 -0500

    Guia de estilos html, main y data

--- src/data.js
+++ src/data.js
@@ -1,115 +1,114 @@
 
 window.computeStudentsStats = (laboratoria) => {
-    //arreglo de objetos final
-    let student = [];
+  // arreglo de objetos final
+  let student = [];
 
-    let campus = "";
-    let name = "";
-    let mail = "";
-    let turnoStudent = "";
-    let stats;
-    let status = "";
-    // let completedPercentagE = 0;
-    let completedPercentage = 0;
-    let topics;
-
-
-    for (key in laboratoria){
-        campus = key;
-        const generations = Object.keys(laboratoria[key].generacion);
-        let n = 1;
-        generations.forEach((generation) => {
-            generation = generation;
-
-            students = laboratoria[key].generacion[generation].estudiantes;
-            for (let i = 0; i < students.length; i++) {
-                name = students[i].nombre;
-                mail = students[i].correo;
-                turnoStudent = students[i].turno;
-                let statS = students[i].progreso;
-                completedPercentage = statS.porcentajeCompletado;
-
-                if (completedPercentage<=60) {
-                    status = "Esta estudiante ha estudiado poco";
-                }else if (completedPercentage>=90){
-                    status = "Esta estudiante ha estudiado mucho";
-                } else {
-                    status = "Es una estudiante promedio"
-                }
-                let topics = statS.temas;
-                for (key1 in topics) {
-                    topicS = key1;
-                    // subtopics = topicS
-                    // console.log(topicS);
-                }
-                student.push({
-                    name,
-                    mail,
-                    campus,
-                    generation,
-                    turnoStudent,
-                    stats : {
-                        status,
-                        completedPercentage,
-                        topics:{ //hay que genrara un objeto de cada tema
-                            completedPercentage: 0,
-                            percentageDuration: 0,
-                            subtopics:{
-                                completedPercentage: 0,
-                                type: "",
-                                duration: 0,
-                            },
-                        },
-                    },
-                });
-            }
-            // console.log(student);
-        })
-    }
-    return  student;
-}
+  let campus = '';
+  let name = '';
+  let mail = '';
+  let turnoStudent = '';
+  let stats;
+  let status = '';
+  // let completedPercentagE = 0;
+  let completedPercentage = 0;
+  let topics;
 
 
+  for (key in laboratoria) {
+    campus = key;
+    const generations = Object.keys(laboratoria[key].generacion);
+      generations.forEach((generation) => {
+      generation = generation;
+
+      students = laboratoria[key].generacion[generation].estudiantes;
+      for (let i = 0; i < students.length; i++) {
+        name = students[i].nombre;
+        mail = students[i].correo;
+        turnoStudent = students[i].turno;
+        let statS = students[i].progreso;
+        completedPercentage = statS.porcentajeCompletado;
+
+        if (completedPercentage <= 60) {
+          status = 'Esta estudiante ha estudiado poco';
+        } else if (completedPercentage >= 90) {
+          status = 'Esta estudiante ha estudiado mucho';
+        } else {
+          status = 'Es una estudiante promedio';
+        }
+        let topics = statS.temas;
+        for (key1 in topics) {
+          topicS = key1;
+          // subtopics = topicS
+          // console.log(topicS);
+        }
+        student.push({
+          name,
+          mail,
+          campus,
+          generation,
+          turnoStudent,
+          stats: {
+            status,
+            completedPercentage,
+            topics: { // hay que genrara un objeto de cada tema
+              completedPercentage: 0,
+              percentageDuration: 0,
+              subtopics: {
+                completedPercentage: 0,
+                type: '',
+                duration: 0,
+              },
+            },
+          },
+        });
+      }
+      // console.log(student);
+    });
+  }
+  return student;
+};
 
 
 window.computeGenerationsStats = (laboratoria) => {
-    const generacion = [];
+  const generacion = [];
 
-    let campus = "";
-    let average = "";
-    let count = 0;
+  let campus = '';
+  let average = '';
+  let count = 0;
 
-    for (key in laboratoria){
-        //rellenando propiedad 'campus' con la key de Laboratoria
-        campus = key;
-        //devolviendo un array del objeto de generaciones
-        const generaciones = Object.keys(laboratoria[key].generacion);
-        // console.log(generaciones);
-        generaciones.forEach((generation) => {
-            generation = generation;
-            const students = laboratoria[key].generacion[generation].estudiantes;
-            let result="";
-            for (let i = 0; i < students.length; i++){
-                let statS = students[i].progreso;
-                count = students.length;
-                // console.log(statS);
-            }
-            generacion.push({
-                campus,
-                generation,
-                average,
-                count,
-            })
-        })
-    }
-
-    return generacion;
-}
-
-
-    window.sortStudents = () => { return },
-    window.filterStudents = (infoStudent, search) => {
-        console.log(infoStudent);
-      const busquedaResultado = infoStudent.filter(name => (infoStudent.name === search));
-    return busquedaResultado;
+  for (key in laboratoria) {
+    // rellenando propiedad 'campus' con la key de Laboratoria
+    campus = key;
+    // devolviendo un array del objeto de generaciones
+    const generaciones = Object.keys(laboratoria[key].generacion);
+    // console.log(generaciones);
+    generaciones.forEach((generation) => {
+      generation = generation;
+      const students = laboratoria[key].generacion[generation].estudiantes;
+      let result = '';
+      for (let i = 0; i < students.length; i++) {
+        let statS = students[i].progreso;
+        count = students.length;
+        // console.log(statS);
+      }
+      generacion.push({
+        campus,
+        generation,
+        average,
+        count,
+      });
+    });
   }
+
+  return generacion;
+};
+
+
+window.sortStudents = () => {
+  return;
+},
+window.filterStudents = (infoStudent, search) => {
+  console.log(infoStudent);
+  const busquedaResultado = infoStudent.filter(name => (infoStudent.name === search));
+  return busquedaResultado;
+};
--- src/main.js
+++ src/main.js
@@ -1,60 +1,57 @@
-document.addEventListener('DOMContentLoaded', ()=>{//DOMContentLoaded es un evento
-    realoadJSON();
-    },false)
+document.addEventListener('DOMContentLoaded', ()=> {// DOMContentLoaded es un evento
+  realoadJSON();
+}, false);
 
-const json = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+const json = 'https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
 
 window.realoadJSON = () => {
-
-    fetch(json)
+  fetch(json)
     .then(response => response.json())
     .then((res) => {
-        const infoStudent = computeStudentsStats(res);
-        const infoGeneration = computeGenerationsStats(res);
-        filterStudents(infoStudent);
-        studentDraw(infoStudent);
-        generationDraw(infoGeneration);
-    })
-    // .catch((error) => {
-    //     console.log(error)
-    // });
-}
+      const infoStudent = computeStudentsStats(res);
+      const infoGeneration = computeGenerationsStats(res);
+      filterStudents(infoStudent);
+      studentDraw(infoStudent);
+      generationDraw(infoGeneration);
+    });
+  // .catch((error) => {
+  //     console.log(error)
+  // });
+};
 
-//pintar tabla de estudiantes
-let listaEstudiantes = document.getElementById("tablaEstudiantes");
+// pintar tabla de estudiantes
+let listaEstudiantes = document.getElementById('tablaEstudiantes');
 
-//Variable que pintara la lista de estudiantes.
-let listaResult = document.getElementById("lista_estudiantes");
-let containerG = document.getElementById("contenido_general");
-let carrusel = document.getElementById("carrusel");
-let search = document.getElementById("search");
+// Variable que pintara la lista de estudiantes.
+let listaResult = document.getElementById('lista_estudiantes');
+let containerG = document.getElementById('contenido_general');
+let carrusel = document.getElementById('carrusel');
+let search = document.getElementById('search');
 
 
+// llamados id de links
+let clickEstudiantesTodos = document.getElementById('estudiantes');
+let clickEstudiantesC4 = document.getElementById('estudiantesC4');
+let clickEstudiantesC5 = document.getElementById('estudiantesC5');
+let clickEstudiantesC6 = document.getElementById('estudiantesC6');
+let clickEstudiantesL4 = document.getElementById('estudiantesL4');
+let clickEstudiantesL5 = document.getElementById('estudiantesL5');
+let clickEstudiantesL6 = document.getElementById('estudiantesL6');
+let clickEstudiantesS4 = document.getElementById('estudiantesS4');
+let clickEstudiantesS5 = document.getElementById('estudiantesS5');
+let clickEstudiantesS6 = document.getElementById('estudiantesS6');
 
-//llamados id de links
-let clickEstudiantesTodos = document.getElementById("estudiantes");
-let clickEstudiantesC4 = document.getElementById("estudiantesC4");
-let clickEstudiantesC5 = document.getElementById("estudiantesC5");
-let clickEstudiantesC6 = document.getElementById("estudiantesC6");
-let clickEstudiantesL4 = document.getElementById("estudiantesL4");
-let clickEstudiantesL5 = document.getElementById("estudiantesL5");
-let clickEstudiantesL6 = document.getElementById("estudiantesL6");
-let clickEstudiantesS4 = document.getElementById("estudiantesS4");
-let clickEstudiantesS5 = document.getElementById("estudiantesS5");
-let clickEstudiantesS6 = document.getElementById("estudiantesS6");
+let clickCdmx = document.getElementById('cdmx');
+let clickLima = document.getElementById('lima');
+let clickSantiago = document.getElementById('santiago');
 
-let clickCdmx = document.getElementById("cdmx");
-let clickLima = document.getElementById("lima");
-let clickSantiago = document.getElementById("santiago");
-
-let clickbtnSearch = document.getElementById("btnSearch");
+let clickbtnSearch = document.getElementById('btnSearch');
 
 window.studentDraw = (infoStudent) => {
-
-    let n = 1;
-    for (let i = 0; i<infoStudent.length; i++) {
-        listaResult.innerHTML += `
-            <tr><th scope="col"> ${n+i}</th>
+  let number = 1;
+  for (let i = 0; i < infoStudent.length; i++) {
+    listaResult.innerHTML += `
+            <tr><th scope="col"> ${number + i}</th>
                 <th scope="col"> ${infoStudent[i].name}</th>
                 <th scope="col"> ${infoStudent[i].mail}</th>
                 <th scope="col"> ${infoStudent[i].turnoStudent}</th>
@@ -62,13 +59,12 @@ window.studentDraw = (infoStudent) => {
                 <th scope="col"> ${infoStudent[i].campus}</th>
             </tr>
             ` ;
-    }
-}
+  }
+};
 
 window.generationDraw = (infoGeneration) => {
-
-    for (let i = 0; i<infoGeneration.length; i++) {
-        containerG.innerHTML += `
+  for (let i = 0; i < infoGeneration.length; i++) {
+    containerG.innerHTML += `
         <div id="cardColor" class="card">
             <div class="info">
                 <p>Sede: ${infoGeneration[i].campus}</p>
@@ -76,24 +72,22 @@ window.generationDraw = (infoGeneration) => {
                 <p>% LMS: ${infoGeneration[i].average}</p>
                 <p># de Estudiantes: ${infoGeneration[i].count}</p>
             </div>
-        </div> `
-    }
-}
+        </div> `;
+  }
+};
 
 
-//evento para jalar lista de estudiantes
-clickEstudiantesTodos.addEventListener("click", (event) => {
-
-    carrusel.style.display="none";
-    containerG.style.display = "none";
-    listaEstudiantes.style.display = "block";
+// evento para jalar lista de estudiantes
+clickEstudiantesTodos.addEventListener('click', (event) => {
+  carrusel.style.display = 'none';
+  containerG.style.display = 'none';
+  listaEstudiantes.style.display = 'block';
 });
 
-clickCdmx.addEventListener("click", (event) => {
-
-    carrusel.style.display="none";
-    listaEstudiantes.style.display = "none";
-    containerG.style.display = "block";
+clickCdmx.addEventListener('click', (event) => {
+  carrusel.style.display = 'none';
+  listaEstudiantes.style.display = 'none';
+  containerG.style.display = 'block';
 });
 
 // clickLima.addEventListener("click", (event) => {
