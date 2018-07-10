20ffd2c1a22ec30abad87bd65b6e46f0c1d7d0b3
Josce Jaimes <nanayhachi94@gmail>
2018-07-09 22:30:45 -0500

    generando cambios en las sedes

--- src/data.js
+++ src/data.js
@@ -1,34 +1,90 @@
-window.global = {
-  //Se genera una función y dentro de ella una constante con la url del JSON para llamarlo
-  computeStudentsStats: (laboratoria) => {
-    const url = `https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`;
-    fetch(url)
-      .then(respuesta => respuesta.json())
-      .then(data => {
-        const generaciones = data.lima.generacion;
-        estudiante(data)
-      })
-  }
-};
 
-estudiante = (data) => {
-  let estudiante = data.lima.generacion.cuarta.estudiantes;
-  let result = "";
-  for (let i = 0; i < estudiante.length; i++) {
-    let nombres = estudiante[i];
-    //console.log(nombres, pintar);
-    result = result + `
+const container = document.getElementById('result'); 
+ 
+laboratoria = (data) =>{
+  document.getElementById('boton1').addEventListener('click', (event) => {
+    let estudiante = data.lima.generacion.cuarta.estudiantes;
+    let progress = estudiante.progreso;
+    let result = '';
+    for (progress of estudiante) {
+      let pro = progress.progreso;
+      console.log(pro);
+      
+        for (let i = 0; i < estudiante.length; i++) {
+          let nombres = estudiante[i];
+          result += `   
           <tr>
             <th scope="row"></th>
-            <td>${nombres.correo}</td>
             <td>${nombres.nombre}</td>
+            <td>${nombres.correo}</td>
             <td>${nombres.turno}</td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-`
-  }
-  //Impresión del resultado
-  pintar.innerHTML = result;
-}
+            <td>${pro.duracionPrograma}</td>
+            <td>${pro.porcentajeCompletado}</td>
+           </tr>
+          
+                         `;
+        }
+    
+  
+        container.innerHTML = result;
+        return result;
+      
+    }
+  }),
+
+
+  document.getElementById('boton2').addEventListener('click', (event) => {
+    let estudiante = data.santiago.generacion.cuarta.estudiantes;
+    let progress = estudiante.progreso;
+    let result = '';
+    for (progress of estudiante) {
+      let pro = progress.progreso;
+      console.log(pro);
+      for (let i = 0; i < estudiante.length; i++) {
+        let nombres = estudiante[i];
+        result += `<div class="card">
+            <div class = "estudiante">
+          
+            <p>nombre: ${nombres.nombre}</p>
+            <p>Correo: ${nombres.correo}</p>
+            <p>Turno: ${nombres.turno}</p>
+            <p>Duracion del programa: ${pro.duracionPrograma}</p>
+            <p>Porcentaje completado: ${pro.porcentajeCompletado}</p>
+            // <p>Temas: ${pro.temas}</p>
+            </div>
+            </div> `;
+      }
+      container.innerHTML = result;
+      return result;
+    };
+  }),
+
+
+  document.getElementById('boton3').addEventListener('click', (event) => {
+    let estudiante = data.mexico.generacion.cuarta.estudiantes;
+    let progress = estudiante.progreso;
+    let result = '';
+    let porcentProgress = '';
+    let durationOfProgram = '';
+    for (progress of estudiante) {
+      let pro = progress.progreso;
+      // console.log(pro);
+      for (let i = 0; i < estudiante.length; i++) {
+        let nombres = estudiante[i];
+        console.log(nombres);
+        result += `<div class="card">
+          <div class = "estudiante">
+        
+          <p>nombre: ${nombres.nombre}</p>
+          <p>Correo: ${nombres.correo}</p>
+          <p>Turno: ${nombres.turno}</p>
+          <p>Duracion del programa: ${pro.duracionPrograma}</p>
+          <p>Porcentaje completado: ${pro.porcentajeCompletado}</p>
+          </div>
+          </div> `;
+      }
+      container.innerHTML = result;
+      return result;
+    }
+  });
+};
