adbd1c8ef4072530742c64e587bee5dba7dfc3df
VaniushaR <vaniusha.ra@gmail.com>
2018-07-11 09:48:09 -0500

    last merge ...

--- src/data.js
+++ src/data.js
@@ -1,40 +1,116 @@
+console.log('conectado');
+const showResult = document.getElementById('result-list');
 
-let clicking = function(){
-fetch(datos).then(function(enlace){
-return datos.json();
-}).then(function(sedes){
-    console.log(sedes.lima[0].generacion.tercera);
-    let gen = sedes.lima[0].generacion.tercera;
-    console.log(gen);
-    let gen2 = sedes.lima[0].generacion.cuarta;
-    console.log(gen2);
-})
-console.log(datos);
-}
-/*computeStudentsStats: (laboratoria) => {
+// Definición de la primera función 
+window.computeStudentsStats = (laboratoria) => {
+  let studentLab = [];
+  let name;
+  let email;
+  let averageSt;
+  let statusSt;
+  let numberG;
+  let topicsSt;
+  let turn;
 
+  for (city in laboratoria) { // primer iteración con for in
+    let sede = city;
+    const generations = Object.keys(laboratoria[city].generacion);
+    // console.log(generations);
+    generations.forEach((genNumber) => { // 1º for each para extraer los valores de cada generación
+      numberG = genNumber;
+      const students = laboratoria[city].generacion[genNumber].estudiantes;
+      // console.log(students);
+      students.forEach((student) => {
+        name = student.nombre;
+        email = student.correo; 
+        turn = student.turno;
+        // console.log(turn);
+        averageSt = student.progreso.porcentajeCompletado; 
+        // condición para valorar el avance de la estudiante
+        if (averageSt < 60) {
+          statusSt = 'below'; // Si está debajo del 60%, aparecerá "below"
+        } else if (averageSt > 90) {
+          statusSt = 'over'; // Si  está sobre el 90%, aparecerá "over"
+        } else {
+          statusSt = 'average'; // Si está dentro de la media, aparecerá "average"
+        };
+        // console.log(averageSt);
+        const topics = Object.keys(student.progreso.temas);
+        // console.log(topics);
+        for (topic of topics) {
+          let topicsRoute = Object.defineProperty(student.progreso.temas, topic, { writable: true });
+          topicsSt = topicsRoute;
+          // console.log(topicsSt);
+          studentProgressTopic = Object.values(topicsSt);
+          for (i = 0; i < studentProgressTopic.length; i++) { 
+            // console.log(studentProgressTopic)
+            studentProgressTopic[i].completedPercentage = studentProgressTopic[i].porcentajeCompletado;
+            let topicProgress = (studentProgressTopic[i].duracionTemaCompletado * 100) / studentProgressTopic[i].duracionTema;
+            studentProgressTopic[i].percentageDuration = Math.round(topicProgress);
+            studentProgressTopic[i].subtopics = studentProgressTopic[i].subtemas;
+          }
+        };
+        // agregar objetos al arreglo student
+        studentLab.push({
+          'name': name,
+          'email': email,
+          'campus': sede,
+          'generation': numberG,
+          'turn': turn,
+          'stats': {
+            'status': statusSt,
+            'completedPercentage': averageSt,
+            'topics': topicsSt
+          }
+        }); // console.log(studentLab);
+      });
+    });
+  }
+  // console.log(studentLab);
+  printerComputeStudentsStats(studentLab);
+  return studentLab;
 };
+// SEGUNDA FUNCIÓN:
+window.computeGenerationsStats = (laboratoria) => {
+  // arreglo generation
+  let generations = [];
+  // objetos generation
+  let campus = '';
+  let generation = '';
+  let count = '';
+  // Variables de un uso: 
+  let eachGenPercent = 0;
+  let totalStudentPerGeneration = 0;
 
-computeGenerationsStats: (laboratoria) => {
-
+  // arreglo de objetos
+  for (sedes in laboratoria) {
+    campus = sedes;
+    const gen = Object.keys(laboratoria[sedes].generacion);
+    for (let numberG of gen) {
+      generation = numberG;
+      const stud = laboratoria[sedes].generacion[numberG].estudiantes;
+      let totalStud = stud.length;
+      count = totalStud;
+      for (eachStudent in stud) {
+        eachGenPercent += laboratoria[sedes].generacion[numberG].estudiantes[eachStudent].progreso.porcentajeCompletado;
+      }
+      totalStudentPerGeneration += count;
+      let totalAverage = Math.round(eachGenPercent / totalStudentPerGeneration);
+      generations.push({'campus': campus,
+        'generation': generation,
+        'average': totalAverage,
+        'count': count
+      }); 
+    }
+  }
+  console.log(generations);
+  printercomputeGenerationsStats(generations);
+  return generations; 
+  // se cierra el segundo for in al nivel generaciones
 };
-
-sortStudents: (students, orderBy, orderDirection) => {
-
-};
-
-/* La función sortStudents() se encarga de ordenar la lista de estudiantes creada con computeStudentsStats() 
-en base a orderBy y orderDirection.
-
-Argumentos
-
-students: Arreglo de objetos creado con computeStudentsStats().
-orderBy: String que indica el criterio de ordenado. Debe permitir ordenar por nombre y porcentaje de completitud.
-orderDirection: La dirección en la que queremos ordenar. Posibles valores: ASC y DESC (ascendente y descendente).
-Valor de retorno
-
-Arreglo de estudiantes ordenado.
-
-filterStudents: (students, search) => {
-
-}; */
+    
+    
+// se cierra primer fun in al nivel sedes
+// }
+    
+// }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,139 @@
-// Llamar a la base de datos en objeto json
-let datos = "https://raw.githubusercontent.com/VaniushaR/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
-// Declarar variables globales
-let sede = document.getElementById("sede");
-let generacion = document.getElementById("generación");
-let nombre = document.getElementById("nombre");
-let btn = document.getElementById("btn");
+// const host = '../data/laboratoria.json';
+const host = 'https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
+console.log(host);
+
+// LOGIN PROVISIONAL
+let enter = document.getElementById('login');
+let userSingin = document.getElementById('buttonLogin');
+const welcomeMessage = document.getElementById('welcome1');
+
+userSingin.addEventListener('click', event => {
+  let user = document.getElementById('user').value;
+  let password = document.getElementById('password').value;
+  if (user === 'lucile@laboratoria.la' && password === 'Devendra') {
+    enter.style.display = 'none';
+    const welcome = `<h2> 
+    ¡Bienvenida! </h2>`;
+    welcome1.innerHTML = welcome;
+  } else {
+    alert(
+      `Password or User Name incorrect. 
+            If you forget your password please contact us at email support@Mich&Vania.com
+                
+                  Have a nice day.
+                  Best rewards, Mich&Vania Co. `);
+  }
+});
+// LOG OUT
+const logOut = document.getElementById('exit');
+logOut.addEventListener('click', event => {
+  container.style.display = 'none';
+});
 
 
-//Llamar al DOM con click
-btn.addEventListener("click", function (){
-    clicking();
-})
+// se inserta dentro del objeto window con método onload para que cargue inmediatamente entrando
+window.onload = () => {
+  fetch(host)
+  // obtenemos la traducción del host a .json para que sea leído
+    .then(response => response.json())
+  // haremos una segunda promesa que guarda en data el response.json
+    .then((laboratoria) => {
+      computeStudentsStats(laboratoria);
+      computeStudentsStats();
+      computeGenerationsStats(laboratoria);
+      computeGenerationsStats();
+      // createInfo es una función que se declara en el data.js para crear elementos en DOM
+    })
+    .catch((error) => {
+      console.log(error);
+      // .catch buscará errores en el enlace FETCH
+    });
+};
 
+// FUNCIONES PARA PINTAR DATOS DE LA SEGUNDA FUNCIÓN:
+const container = document.getElementById('result');
 
+printerComputeStudentsStats = (studentLab) => {
+  document.getElementById('btn').addEventListener('click', (event) => {
+    // console.log(studentLab);
+    let result = '';
 
-//Llamar a los arreglos generaciones depende la sede elegida anteriormente
+    // console.log(Object.values(studentLab));
+    const totalInfo = Object.values(studentLab);
+    // console.log(totalInfo);
+    for (let i = 0; i < totalInfo.length; i++) {
+      // console.log(totalInfo[i]);
+      result += `<div class='table-responsive'>
+      <table class='table'>
+        <thead>
+          <tr>
+            <th> Photo </th>
+            <th> Full Name </th>
+            <th> Email </th>
+            <th> Generation </th>
+            <th> Turn </th>
+            <th> Campus </th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td> <img src='../images/venussymbol.jpg' id='perfil-image'> </td>
+              <td> ${totalInfo[i].name}</td>
+              <td> ${totalInfo[i].email}</td>
+              <td> ${totalInfo[i].generation}</td>
+              <td> ${totalInfo[i].turn}</td>
+              <td> ${totalInfo[i].campus}</td>
+            </tr>
+          </tbody>
+          </table>
+          </div>`;
+    
+      // console.log(result);
+      console.log(container);
+    
+      container.innerHTML = result;
+    }
+  });
+};
+
+document.getElementById('delete').addEventListener('click', (event) =>{
+  container.innerHTML = '';
+});
+
+printercomputeGenerationsStats = (generations) => {
+  document.getElementById('btnG').addEventListener('click', (event) => {
+    let result = '';
+    const totalInfoG = Object.values(generations);
+    for (let i = 0; i < generations.length; i++) {
+      result += `<div class='table-responsive'>
+      <table class='table'>
+        <thead> Progress by Generations
+          <tr>
+            <th> Generation </th>
+            <th> Campus </th>
+            <th> Total Students </th>
+            <th> Average </th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td> ${totalInfoG[i].generation}</td>
+              <td> ${totalInfoG[i].campus}</td>
+              <td> ${totalInfoG[i].count}</td>
+              <td> ${totalInfoG[i].average}</td>
+            </tr>
+          </tbody>
+          </table>
+          </div>`;
+      // console.log(result);
+      console.log(container2);
+    
+      container2.innerHTML = result;
+    }
+  });
+};
+
+document.getElementById('delete').addEventListener('click', (event) =>{
+  container2.innerHTML = '';
+});
+    
