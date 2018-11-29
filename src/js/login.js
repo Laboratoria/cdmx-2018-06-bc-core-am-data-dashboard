87294afa6d9b17b7c95f7a771b2e5b363ff1d0cb
MirEnovi <garciaivonne11@gmail.com>
2018-11-29 14:49:02 -0600

    save change

--- src/js/login.js
+++ src/js/login.js
@@ -1,19 +1,14 @@
 
 // funcion para el inicio de sesion
 window.abrirPagina = () => {
-  let user=document.getElementById("name").value;
-  let loginUser=document.getElementById("login").value;
-  // console.log(user);
-  // console.log(loginUser);
+  let user = document.getElementById('name').value;
+  let loginUser = document.getElementById('login').value;
 
-  if( user === "admin" && loginUser === "123admin"){
 
-    window.open("index_1.html");
-    window.close("index.html");
-
+  if (user === 'admin' && loginUser === '123admin') {
+    window.open('index_1.html');
+    window.close('index.html');
   } else {
-
-    alert("Revisa bien tus datos");
+    alert('Revisa bien tus datos');
   }
-
-}
+};
