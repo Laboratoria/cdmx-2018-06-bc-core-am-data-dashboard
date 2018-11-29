5ea8ccfc536d9e397155ef38900fb909a3a9080d
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-11-28 18:03:57 -0600

    Resolviendo problemas con paso de parametros a funcion dentro de templete string

--- src/JS/main.js
+++ src/JS/main.js
@@ -287,7 +287,7 @@ const listeners = (data) => {
     const selectorSpace = `<div class = "container">
                                     <div class = "row">
                                         <div class="input-field col s11 m5 l5">
-                                            <select onChange = 'changeValue(${students})' name = "orderBy" id = "orderBy">
+                                            <select onChange = 'changeValue()' name = "orderBy" id = "orderBy">
                                             <option value="" disabled selected>Ordenar por: </option>
                                             <option value="name">Nombre</option>
                                             <option value="percentage">Completitud</option>
@@ -295,7 +295,7 @@ const listeners = (data) => {
                                             <label>Categoría para ordenar</label>
                                         </div>
                                         <div class="input-field col s11 m5 l5">
-                                            <select onChange="changeValue2()" name = "orderIn" id = "orderIn">
+                                            <select onChange='changeValue2()' name = "orderIn" id = "orderIn">
                                             <option value="" disabled selected>Ordenar de forma:</option>
                                             <option value="ASC">Ascendente</option>
                                             <option value="DESC">Descendente</option>
@@ -322,7 +322,6 @@ const changeValue = (students) => {
   const orderByTag = document.getElementsByTagName('option');
   const ordering = orderByDetected.value;
   console.log(ordering);
-  console.log(students);
   const orderDirection = 'ASC';
   const sort = sortStudents(students, ordering, orderDirection);
   printSort(sort);
