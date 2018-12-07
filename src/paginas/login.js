4b96e95a4fe231af287758d71476afabbc78d1c6
emeraldng <ifglitterthen@gmail.com>
2018-12-06 20:52:01 -0600

    removiendo imagenes

--- src/paginas/login.js
+++ src/paginas/login.js
@@ -1,5 +1,5 @@
 function usrpas() {
-  if (document.form1.txt.value == "Lucile" && document.form1.num.value == "123") {
+  if (document.form1.txt.value == "Lucile" && document.form1.num.value == 123) {
     window.location = "../index.html"
   } else {
     alert("Error en Usuario o Contraseña. Intenta de nuevo.")
