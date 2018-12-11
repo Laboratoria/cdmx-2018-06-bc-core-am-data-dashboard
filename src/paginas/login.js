22e3c7ec473a136f3380b8caef12cb1b2f2ae5e8
emeraldng <ifglitterthen@gmail.com>
2018-12-11 12:37:18 -0600

    alert con contraseñas

--- src/paginas/login.js
+++ src/paginas/login.js
@@ -1,9 +1,13 @@
 function usrpas() {
-  if (document.form1.txt.value == "Lucile" && document.form1.num.value == 123) {
-    window.location = "../index.html"
+  if (document.form1.txt.value === 'Staff' && document.form1.num.value === 123) {
+    window.location = '../index.html';
   } else {
-    alert("Error en Usuario o Contraseña. Intenta de nuevo.")
+    alert('Error en Usuario o Contraseña. Intenta de nuevo.');
   }
 }
-document.oncontextmenu = new Function("return false");
+document.oncontextmenu = new Function('return false');
 
+
+function password() {
+  alert('Usuario: staff ' + '\nContraseña: 123');
+}
